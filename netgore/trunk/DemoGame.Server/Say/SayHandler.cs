06edf294eee693c11ec98778f54f86e1b0d891fd
Existance0 <Existance0@gmail.com>
2009-02-18 05:32:16 +0000

    fixed a small typo

--- netgore/trunk/DemoGame.Server/Say/SayHandler.cs
+++ netgore/trunk/DemoGame.Server/Say/SayHandler.cs
@@ -66,7 +66,7 @@ namespace DemoGame.Server
                 }
                 using (PacketWriter pw = ServerPacket.ChatTell(user.Name, _message))
                 {
-                    Target.Send(pw);
+                    _target.Send(pw);
                 }
 
             }
