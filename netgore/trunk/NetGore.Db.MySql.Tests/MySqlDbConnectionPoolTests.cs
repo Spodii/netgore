4a948178215afbbe893cf13be65a30f969679398
Spodi <spodii@hotmail.com>
2009-02-26 01:29:01 +0000

    Added NetGore.Db.MySql.Tests project

---
+++ netgore/trunk/NetGore.Db.MySql.Tests/MySqlDbConnectionPoolTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using NUnit.Framework;
+
+namespace NetGore.Db.MySql.Tests
+{
+    [TestFixture]
+    public class MySqlDbConnectionPoolTests
+    {
+        readonly string _connectionString = new MySqlConnectionStringBuilder { Server = "localhost", UserID = "root", Database = "demogame", Password = "" }.ToString();
+
+        MySqlDbConnectionPool CreateConnectionPool()
+        {
+            return new MySqlDbConnectionPool(_connectionString);
+        }
+
+        [Test]
+        public void ConnectionOpenTest()
+        {
+            var pool = CreateConnectionPool();
+
+            using (var connPool = pool.Create())
+            {
+                var conn = connPool.Connection;
+                Assert.IsNotNull(conn);
+                Assert.AreEqual(ConnectionState.Open, conn.State);
+            }
+        }
+
+        [Test]
+        public void ConnectionCloseTest()
+        {
+            var pool = CreateConnectionPool();
+
+            IDbConnection conn;
+            using (var connPool = pool.Create())
+            {
+                conn = connPool.Connection;
+            }
+            Assert.AreEqual(ConnectionState.Closed, conn.State);
+        }
+
+        [Test]
+        public void SelectQueryTest()
+        {
+            var pool = CreateConnectionPool();
+
+            using (var connPool = pool.Create())
+            {
+                using (var cmd = connPool.Connection.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT 100 + 500";
+                    using (var r = cmd.ExecuteReader())
+                    {
+                        r.Read();
+                        Assert.AreEqual(600, r[0]);
+                    }
+                }
+            }
+        }
+    }
+}
