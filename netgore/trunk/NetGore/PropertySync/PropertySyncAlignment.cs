ecefcc92358db7055405cfe6272e5b47a1e7da05
Spodi <spodii@hotmail.com>
2009-06-08 10:45:52 +0000

    Added two new PropertySyncs.

---
+++ netgore/trunk/NetGore/PropertySync/PropertySyncAlignment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using NetGore.IO;
+
+namespace NetGore
+{
+    /// <summary>
+    /// Implementation of a PropertySyncBase that handles synchronizing a Alignment.
+    /// </summary>
+    [PropertySyncHandler(typeof(Alignment))]
+    public sealed class PropertySyncAlignment : PropertySyncBase<Alignment>
+    {
+        /// <summary>
+        /// PropertySyncAlignment constructor.
+        /// </summary>
+        /// <param name="bindObject">Object to bind to.</param>
+        /// <param name="p">PropertyInfo for the property to bind to.</param>
+        public PropertySyncAlignment(object bindObject, PropertyInfo p)
+            : base(bindObject, p)
+        {
+        }
+
+        /// <summary>
+        /// When overridden in the derived class, reads a value with the specified name from an IValueReader.
+        /// </summary>
+        /// <param name="name">Name of the value.</param>
+        /// <param name="reader">IValueReader to read from.</param>
+        /// <returns>Value read from the IValueReader.</returns>
+        protected override Alignment Read(string name, IValueReader reader)
+        {
+            return reader.ReadAlignment(name);
+        }
+
+        /// <summary>
+        /// When overridden in the derived class, writes a value to an IValueWriter with the specified name.
+        /// </summary>
+        /// <param name="name">Name of the value.</param>
+        /// <param name="writer">IValueWriter to write to.</param>
+        /// <param name="value">Value to write.</param>
+        protected override void Write(string name, IValueWriter writer, Alignment value)
+        {
+            writer.Write(name, value);
+        }
+    }
+}
---
+++ netgore/trunk/NetGore/PropertySync/PropertySyncColor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Xna.Framework.Graphics;
+using NetGore.IO;
+
+namespace NetGore
+{
+    /// <summary>
+    /// Implementation of a PropertySyncBase that handles synchronizing a Color.
+    /// </summary>
+    [PropertySyncHandler(typeof(Color))]
+    public sealed class PropertySyncColor : PropertySyncBase<Color>
+    {
+        /// <summary>
+        /// PropertySyncColor constructor.
+        /// </summary>
+        /// <param name="bindObject">Object to bind to.</param>
+        /// <param name="p">PropertyInfo for the property to bind to.</param>
+        public PropertySyncColor(object bindObject, PropertyInfo p)
+            : base(bindObject, p)
+        {
+        }
+
+        /// <summary>
+        /// When overridden in the derived class, reads a value with the specified name from an IValueReader.
+        /// </summary>
+        /// <param name="name">Name of the value.</param>
+        /// <param name="reader">IValueReader to read from.</param>
+        /// <returns>Value read from the IValueReader.</returns>
+        protected override Color Read(string name, IValueReader reader)
+        {
+            return reader.ReadColor(name);
+        }
+
+        /// <summary>
+        /// When overridden in the derived class, writes a value to an IValueWriter with the specified name.
+        /// </summary>
+        /// <param name="name">Name of the value.</param>
+        /// <param name="writer">IValueWriter to write to.</param>
+        /// <param name="value">Value to write.</param>
+        protected override void Write(string name, IValueWriter writer, Color value)
+        {
+            writer.Write(name, value);
+        }
+    }
+}
