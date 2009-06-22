bae872f35ea977d654ea392a40151ec4cace9c40
Spodi <spodii@hotmail.com>
2009-06-22 08:28:41 +0000

    Hopefully fixed for when the DrawPosition != Position, but Velocity == Vector.Zero.

--- netgore/trunk/DemoGame.ClientObjs/Entity/EntityInterpolator.cs
+++ netgore/trunk/DemoGame.ClientObjs/Entity/EntityInterpolator.cs
@@ -14,8 +14,11 @@ namespace DemoGame.Client
     /// </summary>
     public class EntityInterpolator
     {
+        /// <summary>
+        /// The absolute value of the greatest velocity of the Entity.
+        /// </summary>
+        Vector2 _greatestVelocity;
         Vector2 _drawPosition;
-
         int _lastTime;
 
         /// <summary>
@@ -27,15 +30,32 @@ namespace DemoGame.Client
         }
 
         /// <summary>
+        /// Updates the Entity's greatest velocity.
+        /// </summary>
+        /// <param name="currentVelocity">Current velocity.</param>
+        void UpdateGreatestVelocity(Vector2 currentVelocity)
+        {
+            currentVelocity = currentVelocity.Abs();
+
+            if (currentVelocity.X > _greatestVelocity.X)
+                _greatestVelocity.X = currentVelocity.X;
+
+            if (currentVelocity.Y > _greatestVelocity.Y)
+                _greatestVelocity.Y = currentVelocity.Y;
+        }
+
+        /// <summary>
         /// Updates the drawing position interpolation.
         /// </summary>
         /// <param name="entity">Entity that this EntityInterpolator is for.</param>
         /// <param name="currentTime">Current game time.</param>
         public void Update(Entity entity, int currentTime)
         {
-            Vector2 position = entity.Position;
+            if (entity == null)
+                throw new ArgumentNullException("entity");
 
-            // TODO: Store the greatest known velocity, and use that when needed if the current velocity == 0
+            Vector2 position = entity.Position;
+            UpdateGreatestVelocity(entity.Velocity);
 
             // Draw position and real position are already equal
             if (position == _drawPosition)
@@ -44,8 +64,22 @@ namespace DemoGame.Client
             int elapsedTime = currentTime - _lastTime;
             _lastTime = currentTime;
 
-            // Update the draw position according to the Entity's velocity
+            // Get the velocity to use
             Vector2 velocity = entity.Velocity;
+            if (velocity.X == 0)
+            {
+                velocity.X = _greatestVelocity.X;
+                if (position.X < _drawPosition.X)
+                    velocity.X *= -1;
+            }
+            if (velocity.Y == 0)
+            {
+                velocity.Y = _greatestVelocity.Y;
+                if (position.Y < _drawPosition.Y)
+                    velocity.Y *= -1;
+            }
+
+            // Get the new position
             Vector2 newPosition = _drawPosition + (velocity * elapsedTime);
 
             // Don't allow the draw position to exceed the real position
