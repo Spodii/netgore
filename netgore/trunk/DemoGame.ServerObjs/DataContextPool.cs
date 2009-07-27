f5b4aacf41749f569024c9ecaf3f9e3965329d75
Spodi <spodii@hotmail.com>
2009-07-27 08:54:31 +0000

    Added database stuff.

---
+++ netgore/trunk/DemoGame.ServerObjs/DataContextPool.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+namespace DemoGame.Db
+{
+    public static class DataContextPool
+    {
+        static string _connectionString;
+
+        static readonly Dictionary<Thread, DemoGameDb> _pool = new Dictionary<Thread, DemoGameDb>();
+        static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public static void Initialize(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        static DemoGameDb CreateNew()
+        {
+            var conn = new MySqlConnection(_connectionString);
+            return new DemoGameDb(conn);
+        }
+
+        public static DemoGameDb GetDataContext()
+        {
+            DemoGameDb output;
+            var thread = Thread.CurrentThread;
+
+            _lock.EnterUpgradeableReadLock();
+            try
+            {
+                if (!_pool.TryGetValue(thread, out output))
+                {
+                    output = CreateNew();
+                    _lock.EnterWriteLock();
+                    _pool.Add(thread, output);
+                    _lock.ExitWriteLock();
+                }
+            }
+            finally
+            {
+                _lock.ExitUpgradeableReadLock();
+            }
+
+            return output;
+        }
+    }
+}
