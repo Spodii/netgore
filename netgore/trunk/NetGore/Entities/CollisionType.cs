ea7ac06eacc310d1372720b24bcc55b09d2403d5
Spodi <spodii@hotmail.com>
2009-09-02 01:47:57 +0000

    I have no idea what TypeConvertBase was for.

--- netgore/trunk/NetGore/Entities/CollisionType.cs
+++ netgore/trunk/NetGore/Entities/CollisionType.cs
@@ -11,14 +11,17 @@ namespace NetGore
         /// A "full", solid box
         /// </summary>
         Full,
+
         /// <summary>
         /// An equally split box where the top-right half-triangle is cut out
         /// </summary>
         TriangleTopRight,
+
         /// <summary>
         /// An equally split box where the top-left half-triangle is cut out
         /// </summary>
         TriangleTopLeft,
+
         /// <summary>
         /// Entity does not support collision detection at all
         /// </summary>
