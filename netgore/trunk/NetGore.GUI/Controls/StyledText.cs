828273667761a86182de084321c3e0f6c92dd41f
Spodi <spodii@hotmail.com>
2009-09-06 03:12:36 +0000

    Added some helper StyledText stuff.

--- netgore/trunk/NetGore.GUI/Controls/StyledText.cs
+++ netgore/trunk/NetGore.GUI/Controls/StyledText.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -5,22 +6,33 @@ using Microsoft.Xna.Framework.Graphics;
 namespace NetGore.Graphics.GUI
 {
     /// <summary>
-    /// Describes a string of text and the style it uses.
+    /// Describes a string of text and the style it uses. This class is immutable.
     /// </summary>
     public class StyledText
     {
+        /// <summary>
+        /// A StyledText with an empty string.
+        /// </summary>
+        public static readonly StyledText Empty = new StyledText(string.Empty, Color.White);
+
         readonly Color _color;
         readonly string _text;
 
         /// <summary>
         /// Gets the color of the text.
         /// </summary>
-        public Color Color { get { return _color; } }
+        public Color Color
+        {
+            get { return _color; }
+        }
 
         /// <summary>
         /// Gets the text.
         /// </summary>
-        public string Text { get { return _text; } }
+        public string Text
+        {
+            get { return _text; }
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="StyledText"/> class.
@@ -34,6 +46,17 @@ namespace NetGore.Graphics.GUI
         }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="StyledText"/> class.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="sourceStyle">The StyledText to copy the style from.</param>
+        public StyledText(string text, StyledText sourceStyle)
+        {
+            _text = text;
+            _color = sourceStyle.Color;
+        }
+
+        /// <summary>
         /// Draws the StyledText to the <paramref name="spriteBatch"/>.
         /// </summary>
         /// <param name="spriteBatch">SpriteBatch to draw the StyledText to.</param>
@@ -42,6 +65,181 @@ namespace NetGore.Graphics.GUI
         public void Draw(SpriteBatch spriteBatch, SpriteFont spriteFont, Vector2 position)
         {
             spriteBatch.DrawString(spriteFont, Text, position, Color);
+        }
+
+        /// <summary>
+        /// Splits the StyledText into multiple pieces using the given <paramref name="separator"/>.
+        /// </summary>
+        /// <param name="separator">An array of Unicode characters that delimit the substrings in this instance,
+        /// an empty array that contains no delimiters, or null.</param>
+        /// <returns>An array containing the pieces of the StyledText split using the given
+        /// <paramref name="separator"/>.</returns>
+        public StyledText[] Split(params char[] separator)
+        {
+            var strings = Text.Split(separator);
+
+            var ret = new StyledText[strings.Length];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new StyledText(strings[i], Color);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Splits the StyledText into multiple pieces using the given <paramref name="separator"/>.
+        /// </summary>
+        /// <param name="separator">An array of Unicode characters that delimit the substrings in this instance,
+        /// an empty array that contains no delimiters, or null.</param>
+        /// <param name="count">The maximum number of substrings to return.</param>
+        /// <returns>An array containing the pieces of the StyledText split using the given
+        /// <paramref name="separator"/>.</returns>
+        public StyledText[] Split(char[] separator, int count)
+        {
+            var strings = Text.Split(separator, count);
+
+            var ret = new StyledText[strings.Length];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new StyledText(strings[i], Color);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Splits the StyledText into multiple pieces using the given <paramref name="separator"/>.
+        /// </summary>
+        /// <param name="separator">An array of Unicode characters that delimit the substrings in this instance,
+        /// an empty array that contains no delimiters, or null.</param>
+        /// <param name="count">The maximum number of substrings to return.</param>
+        /// <param name="options">Specify <see cref="System.StringSplitOptions.RemoveEmptyEntries"/> to omit empty array
+        /// elements from the array returned, or <see cref="System.StringSplitOptions.None"/> to include
+        /// empty array elements in the array returned.</param>
+        /// <returns>An array containing the pieces of the StyledText split using the given
+        /// <paramref name="separator"/>.</returns>
+        public StyledText[] Split(char[] separator, int count, StringSplitOptions options)
+        {
+            var strings = Text.Split(separator, count, options);
+
+            var ret = new StyledText[strings.Length];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new StyledText(strings[i], Color);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Splits the StyledText into multiple pieces using the given <paramref name="separator"/>.
+        /// </summary>
+        /// <param name="separator">An array of strings that delimit the substrings in this string,
+        /// an empty array that contains no delimiters, or null.</param>
+        /// <param name="count">The maximum number of substrings to return.</param>
+        /// <param name="options">Specify <see cref="System.StringSplitOptions.RemoveEmptyEntries"/> to omit empty array
+        /// elements from the array returned, or <see cref="System.StringSplitOptions.None"/> to include
+        /// empty array elements in the array returned.</param>
+        /// <returns>An array containing the pieces of the StyledText split using the given
+        /// <paramref name="separator"/>.</returns>
+        public StyledText[] Split(string[] separator, int count, StringSplitOptions options)
+        {
+            var strings = Text.Split(separator, count, options);
+
+            var ret = new StyledText[strings.Length];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new StyledText(strings[i], Color);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Splits the StyledText into multiple pieces using the given <paramref name="separator"/>.
+        /// </summary>
+        /// <param name="separator">An array of Unicode characters that delimit the substrings in this instance,
+        /// an empty array that contains no delimiters, or null.</param>
+        /// <param name="options">Specify <see cref="System.StringSplitOptions.RemoveEmptyEntries"/> to omit empty array
+        /// elements from the array returned, or <see cref="System.StringSplitOptions.None"/> to include
+        /// empty array elements in the array returned.</param>
+        /// <returns>An array containing the pieces of the StyledText split using the given
+        /// <paramref name="separator"/>.</returns>
+        public StyledText[] Split(char[] separator, StringSplitOptions options)
+        {
+            var strings = Text.Split(separator, options);
+
+            var ret = new StyledText[strings.Length];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new StyledText(strings[i], Color);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Splits the StyledText into multiple pieces using the given <paramref name="separator"/>.
+        /// </summary>
+        /// <param name="separator">An array of strings that delimit the substrings in this string,
+        /// an empty array that contains no delimiters, or null.</param>
+        /// <param name="options">Specify <see cref="System.StringSplitOptions.RemoveEmptyEntries"/> to omit empty array
+        /// elements from the array returned, or <see cref="System.StringSplitOptions.None"/> to include
+        /// empty array elements in the array returned.</param>
+        /// <returns>An array containing the pieces of the StyledText split using the given
+        /// <paramref name="separator"/>.</returns>
+        public StyledText[] Split(string[] separator, StringSplitOptions options)
+        {
+            var strings = Text.Split(separator, options);
+
+            var ret = new StyledText[strings.Length];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new StyledText(strings[i], Color);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Retrieves a substring from this instance. The substring starts at a specified character position.
+        /// </summary>
+        /// <param name="startIndex">The zero-based starting character position of a substring in this instance.</param>
+        /// <returns>A StyledText equivalent to the substring that begins at startIndex in this instance,
+        /// or StyledText with an empty string if startIndex is equal to the length of this instance.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="startIndex"/> is less than zero or
+        /// greater than the length of this instance.</exception>
+        public StyledText Substring(int startIndex)
+        {
+            string s = Text.Substring(startIndex);
+            return new StyledText(s, Color);
+        }
+
+        /// <summary>
+        /// Retrieves a substring from this instance. The substring starts at a specified character position.
+        /// </summary>
+        /// <param name="startIndex">The zero-based starting character position of a substring in this instance.</param>
+        /// <param name="length">The number of characters in the substring.</param>
+        /// <returns>A StyledText equivalent to the substring that begins at startIndex in this instance,
+        /// or StyledText with an empty string if startIndex is equal to the length of this instance.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="startIndex"/> is less than zero or
+        /// greater than the length of this instance -or- startIndex or length is less than zero.</exception>
+        public StyledText Substring(int startIndex, int length)
+        {
+            string s = Text.Substring(startIndex, length);
+            return new StyledText(s, Color);
+        }
+
+        /// <summary>
+        /// Implements operator + for a StyledText and string.
+        /// </summary>
+        /// <param name="l">The StyledText.</param>
+        /// <param name="r">The string.</param>
+        /// <returns>A new StyledText with the concatenated strings of the two arguments.</returns>
+        public static StyledText operator +(StyledText l, string r)
+        {
+            return new StyledText(l.Text + r, l.Color);
         }
     }
 }
---
+++ netgore/trunk/NetGore.Graphics.GUI.Tests/StyledTextTests.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using NUnit.Framework;
+
+namespace NetGore.Graphics.GUI.Tests
+{
+    [TestFixture]
+    public class StyledTextTests
+    {
+        [Test]
+        public void SubstringTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var s2 = s.Substring(5);
+
+            Assert.AreEqual(s.Text, originalString);
+            Assert.AreEqual(Color.Black, s.Color);
+
+            Assert.AreEqual(originalString.Substring(5), s2.Text);
+            Assert.AreEqual(Color.Black, s2.Color);
+        }
+
+        [Test]
+        public void SubstringWithLengthTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var s2 = s.Substring(5, 4);
+
+            Assert.AreEqual(s.Text, originalString);
+            Assert.AreEqual(Color.Black, s.Color);
+
+            Assert.AreEqual(originalString.Substring(5, 4), s2.Text);
+            Assert.AreEqual(Color.Black, s2.Color);
+        }
+
+        [Test]
+        public void OperatorAddTest()
+        {
+            StyledText s = new StyledText("asdf", Color.Black);
+            s += "ff";
+
+            Assert.AreEqual("asdfff", s.Text);
+            Assert.AreEqual(Color.Black, s.Color);
+        }
+
+        [Test]
+        public void OperatorAddTest2()
+        {
+            StyledText s = new StyledText("asdf", Color.Black);
+            var s2 = s + "ff";
+
+            Assert.AreEqual("asdf", s.Text);
+            Assert.AreEqual(Color.Black, s.Color);
+
+            Assert.AreEqual("asdfff", s2.Text);
+            Assert.AreEqual(Color.Black, s2.Color);
+        }
+
+        [Test]
+        public void SplitCharsTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var split = s.Split(' ');
+            var expected = originalString.Split(' ');
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(s.Color, split[i].Color);
+                Assert.AreEqual(expected[i], split[i].Text);
+            }
+        }
+
+        [Test]
+        public void SplitCharsWithCountTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var split = s.Split(new char[] { ' ' }, 2);
+            var expected = originalString.Split(new char[] { ' ' }, 2);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(s.Color, split[i].Color);
+                Assert.AreEqual(expected[i], split[i].Text);
+            }
+        }
+
+        [Test]
+        public void SplitCharsWithOptionsTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var split = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var expected = originalString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(s.Color, split[i].Color);
+                Assert.AreEqual(expected[i], split[i].Text);
+            }
+        }
+
+        [Test]
+        public void SplitStringsWithOptionsTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var split = s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var expected = originalString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(s.Color, split[i].Color);
+                Assert.AreEqual(expected[i], split[i].Text);
+            }
+        }
+
+        [Test]
+        public void SplitStringsWithOptionsAndCountTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var split = s.Split(new string[] { " " }, 4, StringSplitOptions.RemoveEmptyEntries);
+            var expected = originalString.Split(new string[] { " " }, 4, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(s.Color, split[i].Color);
+                Assert.AreEqual(expected[i], split[i].Text);
+            }
+        }
+
+        [Test]
+        public void SplitCharsWithOptionsAndCountTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            var split = s.Split(new char[] { ' ' }, 3, StringSplitOptions.RemoveEmptyEntries);
+            var expected = originalString.Split(new char[] { ' ' }, 3, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(s.Color, split[i].Color);
+                Assert.AreEqual(expected[i], split[i].Text);
+            }
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            Assert.AreEqual(originalString, s.Text);
+            Assert.AreEqual(Color.Black, s.Color);
+        }
+
+        [Test]
+        public void ConstructorCopyStyleTest()
+        {
+            const string originalString = "asdf werljk xov  .qw 120 xcv;z";
+            StyledText s = new StyledText(originalString, Color.Black);
+            StyledText s2 = new StyledText("ffjfjfj", s);
+
+            Assert.AreEqual(originalString, s.Text);
+            Assert.AreEqual("ffjfjfj", s2.Text);
+
+            Assert.AreEqual(Color.Black, s.Color);
+            Assert.AreEqual(Color.Black, s2.Color);
+        }
+    }
+}
