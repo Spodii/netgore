b1f3c9ad1dab20d4e8eea3e4cf3707456f8664e4
Spodi <spodii@hotmail.com>
2009-09-17 20:43:49 +0000

    Somehow, these all missed the last few commits.

--- netgore/trunk/DemoGame.ServerObjs/DbObjs/Interfaces/IAllianceHostileTable.cs
+++ netgore/trunk/DemoGame.ServerObjs/DbObjs/Interfaces/IAllianceHostileTable.cs
@@ -1,34 +1,42 @@
+using System;
 using System.Linq;
-
 namespace DemoGame.Server.DbObjs
 {
-    /// <summary>
-    /// Interface for a class that can be used to serialize values to the database table `alliance_hostile`.
-    /// </summary>
-    public interface IAllianceHostileTable
-    {
-        /// <summary>
-        /// Gets the value of the database column `alliance_id`.
-        /// </summary>
-        AllianceID AllianceID { get; }
+/// <summary>
+/// Interface for a class that can be used to serialize values to the database table `alliance_hostile`.
+/// </summary>
+public interface IAllianceHostileTable
+{
+/// <summary>
+/// Creates a deep copy of this table. All the values will be the same
+/// but they will be contained in a different object instance.
+/// </summary>
+/// <returns>
+/// A deep copy of this table.
+/// </returns>
+IAllianceHostileTable DeepCopy();
 
-        /// <summary>
-        /// Gets the value of the database column `hostile_id`.
-        /// </summary>
-        AllianceID HostileID { get; }
+/// <summary>
+/// Gets the value of the database column `alliance_id`.
+/// </summary>
+DemoGame.Server.AllianceID AllianceID
+{
+get;
+}
+/// <summary>
+/// Gets the value of the database column `hostile_id`.
+/// </summary>
+DemoGame.Server.AllianceID HostileID
+{
+get;
+}
+/// <summary>
+/// Gets the value of the database column `placeholder`.
+/// </summary>
+System.Nullable<System.Byte> Placeholder
+{
+get;
+}
+}
 
-        /// <summary>
-        /// Gets the value of the database column `placeholder`.
-        /// </summary>
-        byte? Placeholder { get; }
-
-        /// <summary>
-        /// Creates a deep copy of this table. All the values will be the same
-        /// but they will be contained in a different object instance.
-        /// </summary>
-        /// <returns>
-        /// A deep copy of this table.
-        /// </returns>
-        IAllianceHostileTable DeepCopy();
-    }
 }
--- netgore/trunk/DemoGame.ServerObjs/DbObjs/Interfaces/ICharacterInventoryTable.cs
+++ netgore/trunk/DemoGame.ServerObjs/DbObjs/Interfaces/ICharacterInventoryTable.cs
@@ -1,34 +1,42 @@
+using System;
 using System.Linq;
-
 namespace DemoGame.Server.DbObjs
 {
-    /// <summary>
-    /// Interface for a class that can be used to serialize values to the database table `character_inventory`.
-    /// </summary>
-    public interface ICharacterInventoryTable
-    {
-        /// <summary>
-        /// Gets the value of the database column `character_id`.
-        /// </summary>
-        CharacterID CharacterID { get; }
+/// <summary>
+/// Interface for a class that can be used to serialize values to the database table `character_inventory`.
+/// </summary>
+public interface ICharacterInventoryTable
+{
+/// <summary>
+/// Creates a deep copy of this table. All the values will be the same
+/// but they will be contained in a different object instance.
+/// </summary>
+/// <returns>
+/// A deep copy of this table.
+/// </returns>
+ICharacterInventoryTable DeepCopy();
 
-        /// <summary>
-        /// Gets the value of the database column `item_id`.
-        /// </summary>
-        ItemID ItemID { get; }
+/// <summary>
+/// Gets the value of the database column `character_id`.
+/// </summary>
+DemoGame.Server.CharacterID CharacterID
+{
+get;
+}
+/// <summary>
+/// Gets the value of the database column `item_id`.
+/// </summary>
+DemoGame.Server.ItemID ItemID
+{
+get;
+}
+/// <summary>
+/// Gets the value of the database column `slot`.
+/// </summary>
+DemoGame.InventorySlot Slot
+{
+get;
+}
+}
 
-        /// <summary>
-        /// Gets the value of the database column `slot`.
-        /// </summary>
-        InventorySlot Slot { get; }
-
-        /// <summary>
-        /// Creates a deep copy of this table. All the values will be the same
-        /// but they will be contained in a different object instance.
-        /// </summary>
-        /// <returns>
-        /// A deep copy of this table.
-        /// </returns>
-        ICharacterInventoryTable DeepCopy();
-    }
 }
