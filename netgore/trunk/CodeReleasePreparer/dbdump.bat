4b9afb5c1b496ff490c38e0f1a1c1be7356e43dd
mr.spodi <spodii@hotmail.com>
2009-11-03 07:15:45 +0000

    Added comments to dbdump.bat

--- netgore/trunk/CodeReleasePreparer/dbdump.bat
+++ netgore/trunk/CodeReleasePreparer/dbdump.bat
@@ -1,1 +1,4 @@
+echo This file is to generate the db.sql dump file
+echo You will probably need to change the database connection strings
+
 mysqldump demogame --user=root --password= --host=localhost --all-tables --routines --create-options > db.sql
