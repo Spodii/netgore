03aab5bd7f4339ac1cf443433f5e6e6d20b29682
mr.spodi <spodii@hotmail.com>
2009-11-05 20:00:51 +0000

    Delete/add to change name case.

---
+++ netgore/trunk/DemoGame.ServerObjs/DbConnectionSettings.cs
@@ -0,0 +1,76 @@
+using System.Linq;
+using MySql.Data.MySqlClient;
+using NetGore;
+
+namespace DemoGame.Server
+{
+    /// <summary>
+    /// Settings for the database connection.
+    /// </summary>
+    public class DbConnectionSettings
+    {
+        readonly string _sqlDatabase;
+        readonly string _sqlHost;
+        readonly string _sqlPass;
+        readonly string _sqlUser;
+
+        /// <summary>
+        /// Gets the Sql database name containing the game information.
+        /// </summary>
+        public string SqlDatabase
+        {
+            get { return _sqlDatabase; }
+        }
+
+        /// <summary>
+        /// Gets the Sql connection host address.
+        /// </summary>
+        public string SqlHost
+        {
+            get { return _sqlHost; }
+        }
+
+        /// <summary>
+        /// Gets the Sql connection password.
+        /// </summary>
+        public string SqlPass
+        {
+            get { return _sqlPass; }
+        }
+
+        /// <summary>
+        /// Gets the Sql user name.
+        /// </summary>
+        public string SqlUser
+        {
+            get { return _sqlUser; }
+        }
+
+        /// <summary>
+        /// ServerSettings constructor.
+        /// </summary>
+        public DbConnectionSettings()
+        {
+            var dic = XmlInfoReader.ReadFile("DbSettings.xml")[0];
+            _sqlUser = dic["MySql.User"];
+            _sqlHost = dic["MySql.Host"];
+            _sqlDatabase = dic["MySql.Database"];
+
+            if (dic.ContainsKey("MySql.Pass"))
+                _sqlPass = dic["MySql.Pass"];
+            else
+                _sqlPass = string.Empty;
+        }
+
+        /// <summary>
+        /// Makes a Sql connection string for the given settings
+        /// </summary>
+        /// <returns>Sql connection string</returns>
+        public string SqlConnectionString()
+        {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder
+            { Database = SqlDatabase, UserID = SqlUser, Password = SqlPass, Server = SqlHost, IgnorePrepare = false };
+            return sb.ToString();
+        }
+    }
+}
