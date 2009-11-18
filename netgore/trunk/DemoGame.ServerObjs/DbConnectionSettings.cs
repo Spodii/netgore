6f8fd064dfc9a97a1041dc19da34938a4217fa2d
mr.spodi <spodii@hotmail.com>
2009-11-18 22:04:24 +0000

    DbSettings.xml is now copied to a place not under version control when the Server runs. Now we can develop without having to change the database connection information. :)

--- netgore/trunk/DemoGame.ServerObjs/DbConnectionSettings.cs
+++ netgore/trunk/DemoGame.ServerObjs/DbConnectionSettings.cs
@@ -1,7 +1,11 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using log4net;
 using MySql.Data.MySqlClient;
 using NetGore;
+using NetGore.IO;
 
 namespace DemoGame.Server
 {
@@ -10,6 +14,8 @@ namespace DemoGame.Server
     /// </summary>
     public class DbConnectionSettings
     {
+        static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         readonly string _sqlDatabase;
         readonly string _sqlHost;
         readonly string _sqlPass;
@@ -17,11 +23,27 @@ namespace DemoGame.Server
         readonly string _sqlUser;
 
         /// <summary>
+        /// The name and suffix of the settings file.
+        /// </summary>
+        const string _settingsFileName = "DbSettings.xml";
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="DbConnectionSettings"/> class.
         /// </summary>
         public DbConnectionSettings()
         {
-            var dic = XmlInfoReader.ReadFile("DbSettings.xml")[0];
+            // Copy over the default settings file to the destination settings file
+            // This way, project developers don't end up constantly overwriting each other's database settings
+            string destSettingsFile = ContentPaths.Build.Settings.Join(_settingsFileName);
+            if (!File.Exists(destSettingsFile))
+            {
+                if (log.IsInfoEnabled)
+                    log.InfoFormat("Settings file for local server settings copied to `{0}`.", destSettingsFile);
+                File.Copy(_settingsFileName, destSettingsFile);
+            }
+
+            // Read the values
+            var dic = XmlInfoReader.ReadFile(destSettingsFile).First();
             _sqlUser = dic["MySql.User"];
             _sqlHost = dic.AsString("MySql.Host", "localhost");
             _sqlDatabase = dic["MySql.Database"];
@@ -70,9 +92,9 @@ namespace DemoGame.Server
         }
 
         /// <summary>
-        /// Makes a Sql connection string for the given settings
+        /// Makes a Sql connection string for the given settings.
         /// </summary>
-        /// <returns>Sql connection string</returns>
+        /// <returns>Sql connection string.</returns>
         public string SqlConnectionString()
         {
             MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder
