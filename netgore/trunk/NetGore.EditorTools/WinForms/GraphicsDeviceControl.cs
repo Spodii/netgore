2677590ec4dec192b30657eb844c35fcd7c6ff3a
mr.spodi <spodii@hotmail.com>
2009-11-23 02:44:04 +0000

    GraphicsDeviceControl no longer constantly redraws in designer.

--- netgore/trunk/NetGore.EditorTools/WinForms/GraphicsDeviceControl.cs
+++ netgore/trunk/NetGore.EditorTools/WinForms/GraphicsDeviceControl.cs
@@ -28,17 +28,6 @@ namespace NetGore.EditorTools.WinForms
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="GraphicsDeviceControl"/> class.
-        /// </summary>
-        protected GraphicsDeviceControl()
-        {
-            // Create the redraw timer
-            Timer t = new Timer { Interval = 1000 / 60 };
-            t.Tick += delegate { Invalidate(); };
-            t.Start();
-        }
-
-        /// <summary>
         /// Gets an IServiceProvider container
         /// </summary>
         public ServiceContainer Services
@@ -54,8 +43,8 @@ namespace NetGore.EditorTools.WinForms
         string BeginDraw()
         {
             // If we have no graphics device we must be running in the designer
-            if (_gds == null)
-                return Text + "\n\n" + GetType();
+            if (DesignMode || _gds == null)
+                return Text + Environment.NewLine + Environment.NewLine + GetType();
 
             // Make sure the graphics device is big enough and has not been lost
             string deviceResetError = HandleDeviceReset();
@@ -153,7 +142,7 @@ namespace NetGore.EditorTools.WinForms
                 }
                 catch (Exception e)
                 {
-                    return "Graphics device reset failed\n\n" + e;
+                    return "Graphics device reset failed" + Environment.NewLine + Environment.NewLine + e;
                 }
             }
 
@@ -168,7 +157,7 @@ namespace NetGore.EditorTools.WinForms
         }
 
         /// <summary>
-        /// Initializes the control
+        /// Initializes the control.
         /// </summary>
         protected override void OnCreateControl()
         {
@@ -180,6 +169,11 @@ namespace NetGore.EditorTools.WinForms
                 // Register the service, so components like ContentManager can find it
                 _services.AddService<IGraphicsDeviceService>(_gds);
 
+                // Create the redraw timer
+                Timer t = new Timer { Interval = 1000 / 60 };
+                t.Tick += delegate { Invalidate(); };
+                t.Start();
+                
                 // Give derived classes a chance to initialize themselves
                 Initialize();
             }
@@ -188,9 +182,9 @@ namespace NetGore.EditorTools.WinForms
         }
 
         /// <summary>
-        /// Handles MouseDown events
+        /// Handles MouseDown events.
         /// </summary>
-        /// <param name="e">Event args</param>
+        /// <param name="e">Event args.</param>
         protected override void OnMouseDown(MouseEventArgs e)
         {
             Focus();
@@ -198,9 +192,9 @@ namespace NetGore.EditorTools.WinForms
         }
 
         /// <summary>
-        /// Redraws the control in response to a WinForms paint message
+        /// Redraws the control in response to a WinForms paint message.
         /// </summary>
-        /// <param name="e">Event args</param>
+        /// <param name="e">Event args.</param>
         protected override void OnPaint(PaintEventArgs e)
         {
             string beginDrawError = BeginDraw();
@@ -224,7 +218,7 @@ namespace NetGore.EditorTools.WinForms
         /// flickering when our OnPaint implementation then immediately draws some
         /// other color over the top using the XNA Framework GraphicsDevice.
         /// </summary>
-        /// <param name="pevent">Event args</param>
+        /// <param name="pevent">Event args.</param>
         protected override void OnPaintBackground(PaintEventArgs pevent)
         {
         }
@@ -234,12 +228,12 @@ namespace NetGore.EditorTools.WinForms
         /// is lost, or if we are running inside the Form designer), we must use
         /// regular System.Drawing method to display a status message.
         /// </summary>
-        /// <param name="graphics">Graphic to paint to</param>
-        /// <param name="text">Text to write</param>
-        protected virtual void PaintUsingSystemDrawing(System.Drawing.Graphics graphics, string text)
+        /// <param name="graphics">Graphic to paint to.</param>
+        /// <param name="text">Text to write.</param>
+        protected void PaintUsingSystemDrawing(System.Drawing.Graphics graphics, string text)
         {
             graphics.Clear(Color.CornflowerBlue);
-
+            
             using (Brush brush = new SolidBrush(Color.Black))
             {
                 using (StringFormat format = new StringFormat())
