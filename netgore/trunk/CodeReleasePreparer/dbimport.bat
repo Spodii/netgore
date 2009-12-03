eaf7f42d89a38b49284e88d45b1b28c42a2bd421
mr.spodi <spodii@hotmail.com>
2009-12-03 00:18:35 +0000

    Added a tiny db dump import bat file.

---
+++ netgore/trunk/CodeReleasePreparer/dbimport.bat
@@ -0,0 +1,4 @@
+echo This file will import the database dump from its default location
+echo You will probably need to change the database connection strings
+
+mysql --user=root --password= --host=localhost demogame < ..\db.sql
