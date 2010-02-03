5e307a73c92ae4d00cf99ec082ed83f96db22c60
mr.spodi <spodii@hotmail.com>
2010-02-03 06:54:45 +0000

    UserStats.UpdateClient() will only even look if stats are different if a stat has even changed.

--- netgore/trunk/DemoGame.Server/World/Map/Entities/Stats/UserStats.cs
+++ netgore/trunk/DemoGame.Server/World/Map/Entities/Stats/UserStats.cs
@@ -37,10 +37,31 @@ namespace DemoGame.Server
         }
 
         /// <summary>
+        /// Is set to true whenever a stat changes. If false, we don't even need to try looking for changed stats
+        /// in <see cref="UserStats.UpdateClient"/>.
+        /// </summary>
+        bool _anyStatsChanged = false;
+
+        /// <summary>
+        /// When overridden in the derived class, handles when an <see cref="IStat{StatType}"/> in this
+        /// <see cref="DynamicStatCollection{StatType}"/> has changed their value.
+        /// </summary>
+        /// <param name="stat">The <see cref="IStat{StatType}"/> whos value has changed.</param>
+        protected override void OnStatChanged(IStat<StatType> stat)
+        {
+            base.OnStatChanged(stat);
+
+            _anyStatsChanged = true;
+        }
+
+        /// <summary>
         /// Synchronizes the User's stat values to the client.
         /// </summary>
         public void UpdateClient()
         {
+            if (!_anyStatsChanged)
+                return;
+
             // Check if any stat values have changed
             var statsToUpdate = _changedStats.GetChangedStats();
             if (statsToUpdate.IsEmpty())
