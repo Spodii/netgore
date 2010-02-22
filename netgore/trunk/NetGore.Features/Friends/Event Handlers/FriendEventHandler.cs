0feb0ca78a241c78f5c4c1653c470078a45bb877
frankbernier <frankbernier@eb897d4e-bd49-11de-bb2d-0d8fa6df8a7d>
2010-02-22 04:47:49 +0000

    Started the implementation of a friendlist. Will continue soon.

---
+++ netgore/trunk/NetGore.Features/Friends/Event Handlers/FriendEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace NetGore.Features.Friends
+{
+    /// <summary>
+    /// Handles an event from a guild related to a guild member.
+    /// </summary>
+    /// <param name="guild">The guild the event is related to.</param>
+    /// <param name="guildMember">The guild member the event is related to.</param>
+    public delegate void FriendEventHandler(IFriend friend);
+}
