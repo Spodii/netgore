d63728759dab3c142e4dbed6f68ed56c0b783a93
aphro.cag <sam_giles2001@yahoo.co.uk>
2010-03-20 15:39:12 +0000

    Updated dbdump and dbimport so it's easier to change the default port number "--port=3306" as a default.

--- netgore/trunk/CodeReleasePreparer/dbdump.bat
+++ netgore/trunk/CodeReleasePreparer/dbdump.bat
@@ -1,4 +1,5 @@
+@echo off
 echo This file is to generate the db.sql dump file
-echo You will probably need to change the database connection strings
-
-mysqldump demogame --user=root --password= --host=localhost --all-tables --routines --create-options > db.sql
+echo You will probably need to change the database connection strings within the .bat file.
+@echo on
+mysqldump demogame --user=root --password= --host=localhost --port=3306 --all-tables --routines --create-options > db.sql
--- netgore/trunk/CodeReleasePreparer/dbimport.bat
+++ netgore/trunk/CodeReleasePreparer/dbimport.bat
@@ -1,4 +1,5 @@
+@echo off
 echo This file will import the database dump from its default location
-echo You will probably need to change the database connection strings
-
-mysql --user=root --password= --host=localhost demogame < ..\db.sql
+echo You will probably need to change the database connection strings in the .bat file.
+@echo on
+mysql --user=root --password= --host=localhost --port=3306 demogame < ..\db.sql
