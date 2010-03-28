ba0431ff5144b3570893f87c3cb1f1cc5e69e8ee
aphro.cag <sam_giles2001@yahoo.co.uk>
2010-03-28 20:34:19 +0000

    Added a few extra classes so everything builds.

---
+++ netgore/trunk/NetGore.Xna.Framework/Graphics/SpriteBlendMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetGore.Xna.Framework.Graphics
+{
+    public enum SpriteBlendMode
+    {
+        None,
+        AlphaBlend,
+        Additive
+    }
+}
