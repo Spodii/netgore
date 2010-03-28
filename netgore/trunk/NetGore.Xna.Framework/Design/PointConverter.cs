ae3411a300fd17c3570568ce46ba67719ddd280c
aphro.cag <sam_giles2001@yahoo.co.uk>
2010-03-28 18:47:51 +0000

    Added NetGore.Xna.Framework.Point.

---
+++ netgore/trunk/NetGore.Xna.Framework/Design/PointConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+
+namespace NetGore.Xna.Framework.Design
+{
+    public class PointConverter : MathTypeConverter
+    {
+        // Methods
+        public PointConverter()
+        {
+            Type type = typeof(Point);
+            base._propertyDescriptions = new PropertyDescriptorCollection(new PropertyDescriptor[] { new FieldPropertyDescriptor(type.GetField("X")), new FieldPropertyDescriptor(type.GetField("Y")) }).Sort(new string[] { "X", "Y" });
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            int[] numArray = MathTypeConverter.ConvertToValues<int>(context, culture, value, 2, new string[] { "X", "Y" });
+            if (numArray != null)
+            {
+                return new Point(numArray[0], numArray[1]);
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == null)
+            {
+                throw new ArgumentNullException("destinationType");
+            }
+            if ((destinationType == typeof(string)) && (value is Point))
+            {
+                Point point2 = (Point)value;
+                return MathTypeConverter.ConvertFromValues<int>(context, culture, new int[] { point2.X, point2.Y });
+            }
+            if ((destinationType == typeof(InstanceDescriptor)) && (value is Point))
+            {
+                Point point = (Point)value;
+                ConstructorInfo constructor = typeof(Point).GetConstructor(new Type[] { typeof(int), typeof(int) });
+                if (constructor != null)
+                {
+                    return new InstanceDescriptor(constructor, new object[] { point.X, point.Y });
+                }
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+
+        public override object CreateInstance(ITypeDescriptorContext context, IDictionary propertyValues)
+        {
+            if (propertyValues == null)
+            {
+                throw new ArgumentNullException("propertyValues", FrameworkResources.NullNotAllowed);
+            }
+            return new Point((int)propertyValues["X"], (int)propertyValues["Y"]);
+        }
+    }
+
+
+}
---
+++ netgore/trunk/NetGore.Xna.Framework/Point.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using NetGore.Xna.Framework.Design;
+
+namespace NetGore.Xna.Framework
+{
+    [Serializable, StructLayout(LayoutKind.Sequential), TypeConverter(typeof(PointConverter))]
+    public struct Point : IEquatable<Point>
+    {
+        public int X;
+        public int Y;
+        private static Point _zero;
+        public static Point Zero
+        {
+            get
+            {
+                return _zero;
+            }
+        }
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public bool Equals(Point other)
+        {
+            return ((this.X == other.X) && (this.Y == other.Y));
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool flag = false;
+            if (obj is Point)
+            {
+                flag = this.Equals((Point)obj);
+            }
+            return flag;
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.X.GetHashCode() + this.Y.GetHashCode());
+        }
+
+        public override string ToString()
+        {
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+            return string.Format(currentCulture, "{{X:{0} Y:{1}}}", new object[] { this.X.ToString(currentCulture), this.Y.ToString(currentCulture) });
+        }
+
+        public static bool operator ==(Point a, Point b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Point a, Point b)
+        {
+            if (a.X == b.X)
+            {
+                return (a.Y != b.Y);
+            }
+            return true;
+        }
+
+        static Point()
+        {
+            _zero = new Point();
+        }
+    }
+
+
+}
