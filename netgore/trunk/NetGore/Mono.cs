b7f9fde806f046cb0c2321dc5a8284e454fc03d7
aphro.cag <sam_giles2001@yahoo.co.uk>
2010-03-28 21:24:28 +0000

    Small static helper class to determine whether we are running using mono or not.  (Don't want to update the project folder yet as mine is a mess.)

---
+++ netgore/trunk/NetGore/Mono.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetGore
+{
+    public static class Mono
+    {
+        public static bool IsRunningMono()
+        {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+    }
+}
