24fd8304cf0e3d4d1997ef877e9a04f227eb6e8f
aphro.cag <sam_giles2001@yahoo.co.uk>
2010-03-28 19:13:38 +0000

    Small error forgot to check.

--- netgore/trunk/NetGore.Xna.Framework/Design/PointConverter.cs
+++ netgore/trunk/NetGore.Xna.Framework/Design/PointConverter.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.ComponentModel.Design.Serialization;
 using System.Globalization;
