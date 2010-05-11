dcc471c6618b41019763eb8a0a92f40311be1250
mr.spodi <spodii@hotmail.com>
2010-05-11 19:55:56 +0000

    Added methods to add. Changed internal types to longs for I/O values (won't be uncommon for a server to pass 4 GB bandwidth usage).

--- netgore/trunk/NetGore/Network/NetStats.cs
+++ netgore/trunk/NetGore/Network/NetStats.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 
 namespace NetGore.Network
 {
     /// <summary>
     /// Provides statistics related to the networking since the program has started.
+    /// This class is fully thread-safe.
     /// </summary>
     public class NetStats
     {
-        readonly static NetStats _global;
+        static readonly NetStats _global;
+        int _conns;
 
-        uint _tcpSent;
-        uint _tcpRecv;
-        uint _udpSent;
-        uint _udpRecv;
-        uint _conns;
+        long _tcpRecv;
+        long _tcpSent;
+        long _udpRecv;
+        long _udpSent;
 
         /// <summary>
         /// Initializes the <see cref="NetStats"/> class.
@@ -48,6 +47,116 @@ namespace NetGore.Network
         }
 
         /// <summary>
+        /// Gets the number of TCP connections that have been made.
+        /// </summary>
+        public int Connections
+        {
+            get { return _conns; }
+        }
+
+        /// <summary>
+        /// Gets the global <see cref="NetStats"/> instance that contains the accumulated counts since the application started.
+        /// </summary>
+        public static NetStats Global
+        {
+            get { return _global; }
+        }
+
+        /// <summary>
+        /// Gets if this is the global <see cref="NetStats"/> instance.
+        /// </summary>
+        public bool IsGlobalNetStats
+        {
+            get { return this == Global; }
+        }
+
+        /// <summary>
+        /// Gets the number of bytes that have been received over the TCP channel.
+        /// </summary>
+        public long TCPRecv
+        {
+            get { return _tcpRecv; }
+        }
+
+        /// <summary>
+        /// Gets the number of bytes that have been sent over the TCP channel.
+        /// </summary>
+        public long TCPSent
+        {
+            get { return _tcpSent; }
+        }
+
+        /// <summary>
+        /// Gets the number of bytes that have been received over the UDP channel.
+        /// </summary>
+        public long UDPRecv
+        {
+            get { return _udpRecv; }
+        }
+
+        /// <summary>
+        /// Gets the number of bytes that have been sent over the UDP channel.
+        /// </summary>
+        public long UDPSent
+        {
+            get { return _udpSent; }
+        }
+
+        /// <summary>
+        /// Adds to the <see cref="NetStats.Connections"/> property value.
+        /// </summary>
+        /// <param name="value">The amount to add.</param>
+        public void AddConnections(int value)
+        {
+            Interlocked.Add(ref _conns, value);
+        }
+
+        /// <summary>
+        /// Adds to the <see cref="NetStats.TCPRecv"/> property value.
+        /// </summary>
+        /// <param name="value">The amount to add.</param>
+        public void AddTCPRecv(long value)
+        {
+            Interlocked.Add(ref _tcpRecv, value);
+        }
+
+        /// <summary>
+        /// Adds to the <see cref="NetStats.TCPSent"/> property value.
+        /// </summary>
+        /// <param name="value">The amount to add.</param>
+        public void AddTCPSent(long value)
+        {
+            Interlocked.Add(ref _tcpSent, value);
+        }
+
+        /// <summary>
+        /// Adds to the <see cref="NetStats.UDPRecv"/> property value.
+        /// </summary>
+        /// <param name="value">The amount to add.</param>
+        public void AddUDPRecv(long value)
+        {
+            Interlocked.Add(ref _udpRecv, value);
+        }
+
+        /// <summary>
+        /// Adds to the <see cref="NetStats.UDPSent"/> property value.
+        /// </summary>
+        /// <param name="value">The amount to add.</param>
+        public void AddUDPSent(long value)
+        {
+            Interlocked.Add(ref _udpSent, value);
+        }
+
+        /// <summary>
+        /// Creates a deep copy of this <see cref="NetStats"/>.
+        /// </summary>
+        /// <returns>A deep copy of this <see cref="NetStats"/>.</returns>
+        public NetStats DeepCopy()
+        {
+            return new NetStats(this);
+        }
+
+        /// <summary>
         /// Resets all of the network stat values. Cannot be used if <see cref="NetStats.IsGlobalNetStats"/> is set.
         /// </summary>
         /// <exception cref="InvalidOperationException">This <see cref="NetStats"/> instance is the <see cref="NetStats.Global"/>
@@ -65,15 +174,6 @@ namespace NetGore.Network
         }
 
         /// <summary>
-        /// Creates a deep copy of this <see cref="NetStats"/>.
-        /// </summary>
-        /// <returns>A deep copy of this <see cref="NetStats"/>.</returns>
-        public NetStats DeepCopy()
-        {
-            return new NetStats(this);
-        }
-
-        /// <summary>
         /// Checks if two <see cref="NetStats"/> contain the same values.
         /// </summary>
         /// <param name="a">The first argument.</param>
@@ -85,9 +185,8 @@ namespace NetGore.Network
             if (a == null || b == null)
                 return false;
 
-            return a._tcpSent == b._tcpSent && a._tcpRecv == b._tcpRecv &&
-                a._udpSent == b._udpSent && a._udpRecv == b._udpRecv &&
-                a._conns == b._conns;
+            return a._tcpSent == b._tcpSent && a._tcpRecv == b._tcpRecv && a._udpSent == b._udpSent && a._udpRecv == b._udpRecv &&
+                   a._conns == b._conns;
         }
 
         /// <summary>
@@ -125,40 +224,5 @@ namespace NetGore.Network
                 _conns = left._conns + right._conns
             };
         }
-
-        /// <summary>
-        /// Gets if this is the global <see cref="NetStats"/> instance.
-        /// </summary>
-        public bool IsGlobalNetStats { get { return this == Global; } }
-
-        /// <summary>
-        /// Gets the global <see cref="NetStats"/> instance that contains the accumulated counts since the application started.
-        /// </summary>
-        public static NetStats Global { get { return _global; } }
-
-        /// <summary>
-        /// Gets the number of bytes that have been sent over the TCP channel.
-        /// </summary>
-        public uint TCPSent { get { return _tcpSent; } }
-
-        /// <summary>
-        /// Gets the number of bytes that have been received over the TCP channel.
-        /// </summary>
-        public uint TCPRecv { get { return _tcpRecv; } }
-
-        /// <summary>
-        /// Gets the number of bytes that have been sent over the UDP channel.
-        /// </summary>
-        public uint UDPSent { get { return _udpSent; } }
-
-        /// <summary>
-        /// Gets the number of bytes that have been received over the UDP channel.
-        /// </summary>
-        public uint UDPRecv { get { return _udpRecv; } }
-
-        /// <summary>
-        /// Gets the number of TCP connections that have been made.
-        /// </summary>
-        public uint Connections { get { return _conns; } }
     }
 }
