81f418ec8991e9fafeaf72f9b0f83c9b9036cd32
aphro.cag <sam_giles2001@yahoo.co.uk>
2010-09-03 00:26:21 +0000

    Documentation for MemoryMap, plus small improvements to help with its abstraction.

--- netgore/trunk/NetGore/AI/MemoryMap.cs
+++ netgore/trunk/NetGore/AI/MemoryMap.cs
@@ -7,9 +7,6 @@ namespace NetGore.AI
 {
     public class MemoryMap
     {
-        // TODO: Documentation
-
-        // Array holding data for each MemoryCell.
 
         //Holds some information about the list.
         const string _rootNodeName = "MemoryMap";
@@ -21,41 +18,58 @@ namespace NetGore.AI
         MemoryCell[,] _memoryCells;
         ushort _minY;
 
-        /// <summary>
-        /// Initializes the <see cref="MemoryMap"/> class.
-        /// </summary>
-        static MemoryMap()
-        {
-            EncodingFormat = GenericValueIOFormat.Binary;
-        }
+        bool _isInitialized = false;
+
 
         /// <summary>
-        /// Constructor for a MemoryMap, uses default value of 32 for MemoryCell dimensions.
+        /// Constructor for a MemoryMap, uses default value of 32 for MemoryCell dimensions. This constructor should be only used in an AI Editor environment.
         /// </summary>
         public MemoryMap()
         {
+            EncodingFormat = GenericValueIOFormat.Binary;
             _cellSize = 32;
         }
 
         /// <summary>
-        /// Constructor for a MemoryMap
+        /// Constructor for a MemoryMap with a set initial size. This constructor should be only used in an AI Editor environment.
         /// </summary>
         /// <param name="cellSize">Defines the dimensions of a MemoryCell.</param>
         public MemoryMap(ushort cellSize)
         {
+            EncodingFormat = GenericValueIOFormat.Binary;
             _cellSize = cellSize;
         }
 
+        /// <summary>
+        /// Constructor that loads an already saved memory map into it's memory.
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="ID"></param>
+        public MemoryMap(ContentPaths content, int ID)
+        {
+            EncodingFormat = GenericValueIOFormat.Binary;
+            LoadMemoryMap(content, ID);
+        }
+
+        /// <summary>
+        /// Returns the default cell size of the <see cref="MemoryMap"/>.
+        /// </summary>
         public ushort CellSize
         {
             get { return _cellSize; }
         }
 
+        /// <summary>
+        /// Returns the number of columns in the <see cref="MemoryMap"/>
+        /// </summary>
         public ushort CellsX
         {
             get { return _cellsX; }
         }
 
+        /// <summary>
+        /// Returns the number of rows in the <see cref="MemoryMap"/>.
+        /// </summary>
         public ushort CellsY
         {
             get { return _cellsY; }
@@ -69,23 +83,35 @@ namespace NetGore.AI
         /// </summary>
         public static GenericValueIOFormat? EncodingFormat { get; set; }
 
+        /// <summary>
+        /// Returns the underlying detailed information about the <see cref="MemoryMap"/>.
+        /// </summary>
         public MemoryCell[,] MemoryCells
         {
             get { return _memoryCells; }
         }
 
+        /// <summary>
+        /// Static class to retrieve the complete file path of a <see cref="MemoryMap"/> using a given <see cref="ContentPaths"/> and AIMap ID.
+        /// </summary>
+        /// <param name="contentPath">Contentpath to find the AIMap.</param>
+        /// <param name="ID">The ID of the map to find.</param>
+        /// <returns></returns>
         static PathString GetFilePath(ContentPaths contentPath, int ID)
         {
             return contentPath.Maps.Join("AIMap" + ID + EngineSettings.DataFileSuffix);
         }
 
         /// <summary>
-        /// Initializes the MemoryMap by setting up the List of MemoryCells.
+        /// Initializes the MemoryMap by setting up the List of MemoryCells.  Before a MemoryMap can be used it must be Initialized.
         /// </summary>
         /// <param name="maxX">X size of map</param>
         /// <param name="minY">Y size of map</param>
         public void Initialize(ushort maxX, ushort minY)
         {
+
+            _memoryCells = null;
+            
             //Store map size values for use by MemoryMap
             _maxX = maxX;
             _minY = minY;
@@ -109,7 +135,12 @@ namespace NetGore.AI
                 }
             }
         }
-
+        
+        /// <summary>
+        /// Loads a <see cref="MemoryMap"/> into the current class.
+        /// </summary>
+        /// <param name="contentPath">The <see cref="ContentPaths"/> to load this new MemoryMap from.</param>
+        /// <param name="ID">The ID of the MemoryMap to load, this should be kept the same as it's corresponding World Map.</param>
         public void LoadMemoryMap(ContentPaths contentPath, int ID)
         {
             var path = GetFilePath(contentPath, ID);
@@ -135,25 +166,12 @@ namespace NetGore.AI
             }
         }
 
+
         /// <summary>
-        /// Gets the total number of cells visited.
+        /// Saves the current <see cref="MemoryMap"/> to a binary file.
         /// </summary>
-        /// <returns>Returns an integer greater than or equal to 0</returns>
-        public int NumberofCellsVisited()
-        {
-            var total = 0;
-
-            for (var X = 0; X < _cellsX; X++)
-            {
-                for (var Y = 0; Y < _cellsY; Y++)
-                {
-                    if (_memoryCells[X, Y].Weight > 0)
-                        ++total;
-                }
-            }
-            return total;
-        }
-
+        /// <param name="contentPath">The <see cref="ContentPaths"/> to save this MemoryMap to.</param>
+        /// <param name="ID">The ID of the MemoryMap, this should be kept the same as it's corresponding World Map.</param>
         public void SaveMemoryMap(ContentPaths contentPath, int ID)
         {
             var path = GetFilePath(contentPath, ID);
@@ -177,12 +195,12 @@ namespace NetGore.AI
         }
 
         /// <summary>
-        /// Gets the number of Ticks that the instantiating Entity has spent at a particular cell containing a given point. 
+        /// Gets the weight specified for a particular cell in a <see cref="MemoryMap"/>.  Use only for Debug information. 
         /// </summary>
-        /// <param name="xPos">X position.</param>
-        /// <param name="yPos">Y position.</param>
-        /// <returns>Integer indicating the amount of ticks spent at this particular cell.</returns>
-        public int TicksLingered(double xPos, double yPos)
+        /// <param name="xPos">X position on map.</param>
+        /// <param name="yPos">Y position on map.</param>
+        /// <returns>Integer indicating the weight of a particular cell.</returns>
+        public int WeightOfCell(double xPos, double yPos)
         {
             var cellX = (ushort)(xPos / _cellSize);
             var cellY = (ushort)(yPos / _cellSize);
@@ -190,6 +208,11 @@ namespace NetGore.AI
             return _memoryCells[cellX, cellY].Weight;
         }
 
+
+        /// <summary>
+        /// Converts the more detailed information held in a <see cref="MemoryMap"/> to a raw byte array.
+        /// </summary>
+        /// <returns>The raw data of this <see cref="MemoryMap"/> in a two dimensional byte array.</returns>
         public byte[,] ToByteArray()
         {
             var temp = new byte[_cellsX,_cellsY];
