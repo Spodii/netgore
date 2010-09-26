a84f851d7a5461a14dd50cbb5135062efe7c5074
mr.spodi <spodii@hotmail.com>
2010-09-26 17:17:56 +0000

    Fixes issue 198

--- netgore/trunk/NetGore/Network/Sockets/IPSocket.cs
+++ netgore/trunk/NetGore/Network/Sockets/IPSocket.cs
@@ -66,7 +66,7 @@ namespace NetGore.Network
         /// </summary>
         public float AverageLatency
         {
-            get { return _conn.AverageRoundtripTime; }
+            get { return _conn.AverageRoundtripTime * 1000f; }
         }
 
         /// <summary>
