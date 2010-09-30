d86bd3e24339cf00b8fc6019604c7dcbaedc2fad
mr.spodi <spodii@hotmail.com>
2010-09-30 18:13:36 +0000

    Started work on the "Tools", which I think might be what will be the major "add-on" aspect of the editor.

---
+++ netgore/trunk/DemoGame.Editor/Tools/ToolBase.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace DemoGame.Editor.AddOns
+{
+    /// <summary>
+    /// The base class for tools in the editor.
+    /// </summary>
+    public abstract class ToolBase : IDisposable
+    {
+        readonly string _name;
+        readonly ToolManager _toolManager;
+        bool _isDisposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolBase"/> class.
+        /// </summary>
+        /// <param name="name">The name of the tool.</param>
+        /// <param name="toolManager">The <see cref="ToolManager"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="name"/> is null or empty.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="toolManager"/> is null.</exception>
+        protected ToolBase(string name, ToolManager toolManager)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+            if (toolManager == null)
+                throw new ArgumentNullException("toolManager");
+
+            _name = name;
+            _toolManager = toolManager;
+        }
+
+        /// <summary>
+        /// Gets if this object has been disposed.
+        /// </summary>
+        public bool IsDisposed
+        {
+            get { return _isDisposed; }
+        }
+
+        /// <summary>
+        /// Gets the name of the tool.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ToolManager"/> that this tool is in.
+        /// </summary>
+        public ToolManager ToolManager
+        {
+            get { return _toolManager; }
+        }
+
+        /// <summary>
+        /// When overridden in the derived class, handles disposing this object.
+        /// </summary>
+        /// <param name="disposeManaged">When true, <see cref="IDisposable.Dispose"/> was called directly and managed resources need to be
+        /// disposed. When false, this object was garbage collected and managed resources should not be disposed.</param>
+        protected virtual void Dispose(bool disposeManaged)
+        {
+        }
+
+        /// <summary>
+        /// Releases unmanaged resources and performs other cleanup operations before the
+        /// <see cref="ToolBase"/> is reclaimed by garbage collection.
+        /// </summary>
+        ~ToolBase()
+        {
+            _isDisposed = true;
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// When overridden in the derived class, gets the <see cref="Image"/> to display for this <see cref="ToolBase"/> in
+        /// the toolbar.
+        /// </summary>
+        /// <returns>The <see cref="Image"/> to display in the toolbar, or null if it cannot be added to the toolbar.</returns>
+        protected virtual Image GetToolbarIcon()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// When overridden in the derived class, handles when the tool is clicked in the toolbar.
+        /// </summary>
+        protected virtual void OnToolbarClicked()
+        {
+        }
+
+        #region IDisposable Members
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (IsDisposed)
+                return;
+
+            GC.SuppressFinalize(this);
+            _isDisposed = true;
+
+            Dispose(true);
+        }
+
+        #endregion
+    }
+}
---
+++ netgore/trunk/DemoGame.Editor/Tools/ToolManager.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using log4net;
+using NetGore;
+using NetGore.Collections;
+
+namespace DemoGame.Editor.AddOns
+{
+    public class ToolManager
+    {
+        static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static readonly ToolManager _instance;
+
+        readonly Dictionary<Type, ToolBase> _tools = new Dictionary<Type, ToolBase>();
+
+        /// <summary>
+        /// Initializes the <see cref="ToolManager"/> class.
+        /// </summary>
+        static ToolManager()
+        {
+            _instance = new ToolManager();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolManager"/> class.
+        /// </summary>
+        ToolManager()
+        {
+            var tfc = new TypeFilterCreator
+            {
+                IsClass = true,
+                IsAbstract = false,
+                RequireConstructor = true,
+                ConstructorParameters = new Type[] { typeof(ToolManager) },
+                Subclass = typeof(ToolBase),
+            };
+
+            var filter = tfc.GetFilter();
+
+            new TypeFactory(filter, TypeFactory_TypeLoaded);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ToolManager"/> instance.
+        /// </summary>
+        public static ToolManager Instance
+        {
+            get { return _instance; }
+        }
+
+        /// <summary>
+        /// Gets all tools available.
+        /// </summary>
+        public IEnumerable<ToolBase> Tools
+        {
+            get { return _tools.Values; }
+        }
+
+        /// <summary>
+        /// Tries to get a <see cref="ToolBase"/> from its <see cref="Type"/>.
+        /// </summary>
+        /// <param name="type">The type of the tool to get.</param>
+        /// <returns>The tool of the given <paramref name="type"/>, or null if not found.</returns>
+        public ToolBase TryGetTool(Type type)
+        {
+            ToolBase ret;
+            if (!_tools.TryGetValue(type, out ret))
+                return null;
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Tries to get a <see cref="ToolBase"/> from its type.
+        /// </summary>
+        /// <typeparam name="T">The type of the tool to get.</typeparam>
+        /// <returns>The tool of type <typeparamref name="T"/>, or null if not found.</returns>
+        public T TryGetTool<T>() where T : ToolBase
+        {
+            var ret = TryGetTool(typeof(T));
+            Debug.Assert(ret is T);
+            return ret as T;
+        }
+
+        /// <summary>
+        /// Handles when a new type has been loaded into a <see cref="TypeFactory"/>.
+        /// </summary>
+        /// <param name="typeFactory"><see cref="TypeFactory"/> that the event occured on.</param>
+        /// <param name="loadedType">Type that was loaded.</param>
+        /// <param name="name">Name of the Type.</param>
+        void TypeFactory_TypeLoaded(TypeFactory typeFactory, Type loadedType, string name)
+        {
+            try
+            {
+                // Instantiate the type
+                ToolBase tool;
+                try
+                {
+                    tool = (ToolBase)TypeFactory.GetTypeInstance(loadedType);
+                }
+                catch (Exception ex)
+                {
+                    const string errmsg = "Failed to instantiate tool type `{0}`. Exception: {1}";
+                    if (log.IsErrorEnabled)
+                        log.ErrorFormat(errmsg, loadedType, ex);
+                    Debug.Fail(string.Format(errmsg, loadedType, ex));
+                    return;
+                }
+
+                Debug.Assert(tool.GetType() == loadedType);
+
+                // Add to the collection
+                try
+                {
+                    _tools.Add(loadedType, tool);
+                }
+                catch (Exception ex)
+                {
+                    const string errmsg = "Failed to add tool `{0}` (type: {1}) to collection. Exception: {2}";
+                    if (log.IsErrorEnabled)
+                        log.ErrorFormat(errmsg, tool, tool.GetType(), ex);
+                    Debug.Fail(string.Format(errmsg, tool, tool.GetType(), ex));
+
+                    tool.Dispose();
+
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                const string errmsg = "Unexpected error while trying to load tool from type `{0}`. Exception: {1}";
+                if (log.IsErrorEnabled)
+                    log.ErrorFormat(errmsg, loadedType, ex);
+                Debug.Fail(string.Format(errmsg, loadedType, ex));
+            }
+        }
+    }
+}
