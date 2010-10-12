ddbf62e4f07e47a69fa5666f491123f849cd0997
mr.spodi <spodii@hotmail.com>
2010-10-12 01:46:50 +0000

    Some other queries I missed updating, and got rid of some of the old query building functions.

--- netgore/trunk/DemoGame.Server/Queries/Character/SelectAccountCharacterInfoQuery.cs
+++ netgore/trunk/DemoGame.Server/Queries/Character/SelectAccountCharacterInfoQuery.cs
@@ -3,20 +3,33 @@ using System.Data.Common;
 using System.Linq;
 using DemoGame.Server.DbObjs;
 using NetGore.Db;
+using NetGore.Db.QueryBuilder;
 
 namespace DemoGame.Server.Queries
 {
     [DbControllerQuery]
     public class SelectAccountCharacterInfoQuery : DbQueryReader<CharacterID>
     {
-        static readonly string _queryStr = FormatQueryString("SELECT `name`,`body_id` FROM `{0}` WHERE `id`=@id",
-                                                             CharacterTable.TableName);
+        /// <summary>
+        /// Creates the query for this class.
+        /// </summary>
+        /// <param name="qb">The <see cref="IQueryBuilder"/> instance.</param>
+        /// <returns>The query for this class.</returns>
+        static string CreateQuery(IQueryBuilder qb)
+        {
+            // SELECT `name`,`body_id` FROM `{0}` WHERE `id`=@id
+
+            var f = qb.Functions;
+            var s = qb.Settings;
+            var q = qb.Select(CharacterTable.TableName).Add("name","body_id").Where(f.Equals(s.EscapeColumn("id"), s.Parameterize("id")));
+            return q.ToString();
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SelectAccountCharacterInfoQuery"/> class.
         /// </summary>
         /// <param name="connectionPool">The connection pool.</param>
-        public SelectAccountCharacterInfoQuery(DbConnectionPool connectionPool) : base(connectionPool, _queryStr)
+        public SelectAccountCharacterInfoQuery(DbConnectionPool connectionPool) : base(connectionPool, CreateQuery(connectionPool.QueryBuilder))
         {
             QueryAsserts.ArePrimaryKeys(CharacterTable.DbKeyColumns, "id");
             QueryAsserts.ContainsColumns(CharacterTable.DbColumns, "name", "body_id");
--- netgore/trunk/DemoGame.Server/Queries/Map/SelectMapIDsQuery.cs
+++ netgore/trunk/DemoGame.Server/Queries/Map/SelectMapIDsQuery.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
-using System.Data.Common;
 using System.Linq;
 using DemoGame.DbObjs;
-using DemoGame.Server.DbObjs;
 using NetGore.Db;
 using NetGore.Db.QueryBuilder;
 using NetGore.World;
@@ -10,16 +8,8 @@ using NetGore.World;
 namespace DemoGame.Server.Queries
 {
     [DbControllerQuery]
-    public class SelectMapQuery : DbQueryReader<MapID>
+    public class SelectMapIDsQuery : DbQueryReader
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SelectMapQuery"/> class.
-        /// </summary>
-        /// <param name="connectionPool">DbConnectionPool to use for creating connections to execute the query on.</param>
-        public SelectMapQuery(DbConnectionPool connectionPool) : base(connectionPool, CreateQuery(connectionPool.QueryBuilder))
-        {
-        }
-
         /// <summary>
         /// Creates the query for this class.
         /// </summary>
@@ -27,62 +17,18 @@ namespace DemoGame.Server.Queries
         /// <returns>The query for this class.</returns>
         static string CreateQuery(IQueryBuilder qb)
         {
-            // SELECT * FROM `{0}` WHERE `id`=@id
+            // SELECT `id` FROM `{0}`
 
-            var f = qb.Functions;
-            var s = qb.Settings;
-            var q = qb.Select(MapTable.TableName).AllColumns().Where(f.Equals(s.EscapeColumn("id"), s.Parameterize("id")));
+            var q = qb.Select(MapTable.TableName).Add("id");
             return q.ToString();
         }
-
-        public IMapTable Execute(MapID id)
-        {
-            MapTable ret;
-
-            using (var r = ExecuteReader(id))
-            {
-                if (!r.Read())
-                    return null;
-
-                ret = new MapTable();
-                ret.ReadValues(r);
-            }
-
-            return ret;
-        }
-
-        /// <summary>
-        /// When overridden in the derived class, creates the parameters this class uses for creating database queries.
-        /// </summary>
-        /// <returns>IEnumerable of all the <see cref="DbParameter"/>s needed for this class to perform database queries.
-        /// If null, no parameters will be used.</returns>
-        protected override IEnumerable<DbParameter> InitializeParameters()
-        {
-            return CreateParameters("id");
-        }
-
-        /// <summary>
-        /// When overridden in the derived class, sets the database parameters values <paramref name="p"/>
-        /// based on the values specified in the given <paramref name="item"/> parameter.
-        /// </summary>
-        /// <param name="p">Collection of database parameters to set the values for.</param>
-        /// <param name="item">The value or object/struct containing the values used to execute the query.</param>
-        protected override void SetParameters(DbParameterValues p, MapID item)
-        {
-            p["id"] = (int)item;
-        }
-    }
-
-    [DbControllerQuery]
-    public class SelectMapIDsQuery : DbQueryReader
-    {
-        static readonly string _queryStr = FormatQueryString("SELECT `id` FROM `{0}`", MapTable.TableName);
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SelectMapIDsQuery"/> class.
         /// </summary>
         /// <param name="connectionPool">DbConnectionPool to use for creating connections to execute the query on.</param>
-        public SelectMapIDsQuery(DbConnectionPool connectionPool) : base(connectionPool, _queryStr)
+        public SelectMapIDsQuery(DbConnectionPool connectionPool)
+            : base(connectionPool, CreateQuery(connectionPool.QueryBuilder))
         {
         }
 
