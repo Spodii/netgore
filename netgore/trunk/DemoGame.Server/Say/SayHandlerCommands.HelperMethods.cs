c5e1ac2da7e00d0f07d68e57b79e779afcebb2a5
mr.spodi <spodii@hotmail.com>
2010-10-13 06:12:51 +0000

    Split up the Say command stuff into multiple partial classes, and applied the new attribute.

---
+++ netgore/trunk/DemoGame.Server/Say/SayHandlerCommands.HelperMethods.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Linq;
+using DemoGame.DbObjs;
+using NetGore.Features.Groups;
+using NetGore.Features.Guilds;
+
+namespace DemoGame.Server
+{
+    /// <summary>
+    /// <see cref="SayHandlerCommands"/> helper methods. Does not contain any actual commands.
+    /// </summary>
+    public partial class SayHandlerCommands
+    {
+        /// <summary>
+        /// Checks if the user meets the required guild rank.
+        /// </summary>
+        /// <param name="requiredRank">The required guild rank.</param>
+        /// <returns>If false, the command should be aborted.</returns>
+        bool CheckGuildPermissions(GuildRank requiredRank)
+        {
+            if (((IGuildMember)User).GuildRank < requiredRank)
+            {
+                User.Send(GameMessage.GuildInsufficientPermissions, ServerMessageType.GUI, GuildSettings.GetRankName(requiredRank));
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Handles kicking someone out of a guild.
+        /// </summary>
+        /// <param name="target">The <see cref="IGuildMember"/> being kicked out of the guild.</param>
+        /// <param name="userName">The name of the <paramref name="target"/>.</param>
+        void GuildMemberPerformer_GuildKick(IGuildMember target, string userName)
+        {
+            if (target == null)
+            {
+                User.Send(GameMessage.GuildKickFailedInvalidUser, ServerMessageType.GUI, userName);
+                return;
+            }
+
+            if (target.Guild != User.Guild)
+            {
+                User.Send(GameMessage.GuildKickFailedNotInGuild, ServerMessageType.GUI, target.Name);
+                return;
+            }
+
+            if (target.GuildRank > ((IGuildMember)User).GuildRank)
+            {
+                User.Send(GameMessage.GuildKickFailedTooHighRank, ServerMessageType.GUI, target.Name);
+                return;
+            }
+
+            if (!User.Guild.TryKickMember(User, target))
+            {
+                User.Send(GameMessage.GuildKickFailedUnknownReason, ServerMessageType.GUI, target.Name);
+                return;
+            }
+
+            User.Send(GameMessage.GuildKick, ServerMessageType.GUI, target.Name);
+        }
+
+        /// <summary>
+        /// Requires the user to not be in a group.
+        /// </summary>
+        /// <returns>If false, the command should be aborted.</returns>
+        bool RequireInGroup()
+        {
+            if (((IGroupable)User).Group == null)
+            {
+                User.Send(GameMessage.InvalidCommandMustBeInGroup, ServerMessageType.GUI);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Requires the user to not be in a group.
+        /// </summary>
+        /// <returns>If false, the command should be aborted.</returns>
+        bool RequireNotInGroup()
+        {
+            if (((IGroupable)User).Group != null)
+            {
+                User.Send(GameMessage.InvalidCommandMustNotBeInGroup, ServerMessageType.GUI);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Requires the user to be in a guild.
+        /// </summary>
+        /// <returns>If false, the command should be aborted.</returns>
+        bool RequireUserInGuild()
+        {
+            if (User.Guild == null)
+            {
+                User.Send(GameMessage.InvalidCommandMustBeInGuild, ServerMessageType.GUI);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Requires the user to not be in a guild.
+        /// </summary>
+        /// <returns>If false, the command should be aborted.</returns>
+        bool RequireUserNotInGuild()
+        {
+            if (User.Guild != null)
+            {
+                User.Send(GameMessage.InvalidCommandMustNotBeInGuild, ServerMessageType.GUI);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Sends a chat message to the <see cref="User"/>. This is provided purely for convenience since it can
+        /// become quite redundant having to constantly create the <see cref="ServerPacket.Chat"/> calls.
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        void UserChat(string message)
+        {
+            using (var pw = ServerPacket.Chat(message))
+            {
+                User.Send(pw, ServerMessageType.GUIChat);
+            }
+        }
+
+        /// <summary>
+        /// Sends a chat message to the <see cref="User"/>. This is provided purely for convenience since it can
+        /// become quite redundant having to constantly create the <see cref="ServerPacket.Chat"/> calls.
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        /// <param name="args">The string formatting arguments.</param>
+        void UserChat(string message, params object[] args)
+        {
+            var msgFormatted = string.Format(message, args);
+            using (var pw = ServerPacket.Chat(msgFormatted))
+            {
+                User.Send(pw, ServerMessageType.GUIChat);
+            }
+        }
+
+        void UserChat(IAccountBanTable banInfo, int index, int total)
+        {
+            UserChat("Ban {0} of {1}:", index, total);
+            UserChat("   BanID: {0}", banInfo.ID);
+            UserChat("   Issued by: {0}", banInfo.IssuedBy);
+            UserChat("   Start: {0}", banInfo.StartTime.ToString("U"));
+            UserChat("   End: {0}", banInfo.EndTime.ToString("U"));
+            UserChat("   Duration: {0}", (banInfo.EndTime - banInfo.StartTime).Duration());
+            UserChat("   Remaining (approx): {0}", (DateTime.Now - banInfo.EndTime).Duration());
+            UserChat("   Reason: {0}", banInfo.Reason);
+        }
+    }
+}
---
+++ netgore/trunk/DemoGame.Server/Say/SayHandlerCommands.LesserAdmin.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Linq;
+using NetGore;
+using NetGore.World;
+using SFML.Graphics;
+
+namespace DemoGame.Server
+{
+    /// <summary>
+    /// <see cref="SayHandlerCommands"/> for <see cref="UserPermissions.LesserAdmin"/>.
+    /// </summary>
+    public partial class SayHandlerCommands
+    {
+        /// <summary>
+        /// Creates an instance of an item from a template and adds it to your inventory. Any items that cannot
+        /// fit into the caller's inventory are destroyed.
+        /// </summary>
+        /// <param name="id">The ID of the item template to use.</param>
+        /// <param name="amount">The number of items to create.</param>
+        [SayHandlerCommand("CreateItem", UserPermissions.LesserAdmin)]
+        public void CreateItem(ItemTemplateID id, byte amount)
+        {
+            // Get the item template
+            var template = ItemTemplateManager.Instance[id];
+            if (template == null)
+            {
+                UserChat("Invalid item template ID: " + id);
+                return;
+            }
+
+            // Create the item
+            var item = new ItemEntity(template, amount);
+
+            // Give to user
+            var remainder = User.Inventory.Add(item);
+
+            // Delete any that failed to be added
+            if (remainder != null)
+            {
+                UserChat(remainder.Amount + " units could not be added to your inventory.");
+                remainder.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Desummons all NPCs on the current map.
+        /// </summary>
+        [SayHandlerCommand("Dethrall", UserPermissions.LesserAdmin)]
+        public void Dethrall()
+        {
+            var userMap = User.Map;
+            if (userMap == null)
+                return;
+
+            // Get the thralled NPCs
+            var toKill = userMap.NPCs.OfType<ThralledNPC>().Where(x => x.IsAlive).ToImmutable();
+
+            // Kill all the found thralled NPCs
+            foreach (var thralledNPC in toKill)
+            {
+                thralledNPC.Kill();
+            }
+        }
+
+        /// <summary>
+        /// Kills the specified user.
+        /// </summary>
+        /// <param name="userName">The player to kill.</param>
+        [SayHandlerCommand("KillUser", UserPermissions.LesserAdmin)]
+        public void KillUser(string userName)
+        {
+            // Get the user we want
+            var target = World.FindUser(userName);
+
+            // Check that the user could be found
+            if (target == null)
+            {
+                User.Send(GameMessage.CommandTellInvalidUser, ServerMessageType.GUIChat);
+                return;
+            }
+
+            target.Kill();
+        }
+
+        /// <summary>
+        /// Warps the specified player to the user of the command.
+        /// </summary>
+        /// <param name="userName">The name of the player to summon.</param>
+        [SayHandlerCommand("Summon", UserPermissions.LesserAdmin)]
+        public void Summon(string userName)
+        {
+            // Get the user we want
+            var target = World.FindUser(userName);
+
+            // Check that the user could be found
+            if (target == null)
+            {
+                User.Send(GameMessage.CommandTellInvalidUser, ServerMessageType.GUIChat);
+                return;
+            }
+
+            // Target user was found, so teleport them to the user that issued the command 
+            target.Teleport(User.Map, User.Position);
+        }
+
+        /// <summary>
+        /// Summons NPCs on the current map.
+        /// </summary>
+        /// <param name="id">The ID of the NPCs to spawn.</param>
+        /// <param name="amount">The amount of NPCs to spawn.</param>
+        [SayHandlerCommand("Thrall", UserPermissions.LesserAdmin)]
+        public void Thrall(CharacterTemplateID id, int amount)
+        {
+            var thrallArea = new Rectangle();
+            var useThrallArea = false;
+
+            // When standing on top of something, also spawn the NPCs on the thing the User is standing on,
+            // and spread them out a bit on it without exceeding the size of it
+            var userStandingOn = User.StandingOn;
+            if (userStandingOn != null)
+            {
+                useThrallArea = true;
+
+                var minX = userStandingOn.Position.X;
+                var maxX = userStandingOn.Max.X;
+                var y = userStandingOn.Position.Y;
+
+                minX = Math.Max(minX, User.Position.X - 96);
+                maxX = Math.Min(maxX, User.Position.X + 96);
+
+                thrallArea = new Rectangle((int)minX, (int)y, (int)(maxX - minX + 1), 1);
+            }
+
+            for (var i = 0; i < amount; i++)
+            {
+                // Create a ThralledNPC and add it to the world
+                var npc = new ThralledNPC(World, CharacterTemplateManager.Instance[id], User.Map, User.Position);
+
+                // When using the thrallArea, move the NPC to the correct area
+                if (useThrallArea)
+                {
+                    var npcSize = npc.Size;
+                    var minX = thrallArea.Left;
+                    var maxX = thrallArea.Right - (int)npcSize.X;
+                    int x;
+
+                    if (maxX <= minX)
+                        x = minX;
+                    else
+                        x = RandomHelper.NextInt(minX, maxX);
+
+                    var y = thrallArea.Y - (int)npc.Size.Y;
+                    npc.Teleport(new Vector2(x, y));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Warps the user to the specified map and position.
+        /// </summary>
+        /// <param name="mapID">The mapID to be warped to.</param>
+        /// <param name="x">The position along the x-axis to be warped to.</param>
+        /// <param name="y">The position along the y-axis to be warped to.</param>
+        [SayHandlerCommand("Warp", UserPermissions.LesserAdmin)]
+        public void Warp(MapID mapID, int x, int y)
+        {
+            // Check for a valid map
+            if (!MapBase.IsMapIDValid(mapID))
+            {
+                UserChat("Invalid map ID: " + mapID);
+                return;
+            }
+
+            // Move the user
+            User.Teleport(World.GetMap(mapID), new Vector2(x, y));
+        }
+    }
+}
