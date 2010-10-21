2bebfdb1dda778425450f5091aa908e508b51347
mr.spodi <spodii@hotmail.com>
2010-10-21 19:56:46 +0000

    Started working on my own implementation of the Settings.settings since Settings.settings doesn't have any decent way for supporting comments, but decided its not worth the effort. Committing only to preserve what I did thus far.

---
+++ netgore/trunk/NetGore/IO/Runtime Settings/RuntimeSettingsEventHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace NetGore.IO
+{
+    /// <summary>
+    /// Delegate for handling events from the <see cref="IRuntimeSettings"/>.
+    /// </summary>
+    /// <param name="sender">The <see cref="IRuntimeSettings"/> the event came from.</param>
+    public delegate void RuntimeSettingsEventHandler(IRuntimeSettings sender);
+}
