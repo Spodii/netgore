76995868d0d3dfe11680309c7784a586c9837759
mr.spodi <spodii@hotmail.com>
2010-10-25 03:35:14 +0000

    Added ability to filter by the current default layer and depth for MapGrhs.

--- netgore/trunk/DemoGame.Editor/MapDrawFilterHelper.cs
+++ netgore/trunk/DemoGame.Editor/MapDrawFilterHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq;
+using DemoGame.Client;
+using DemoGame.Editor.Properties;
 using NetGore;
 using NetGore.Graphics;
 using NetGore.IO;
@@ -12,18 +14,23 @@ namespace DemoGame.Editor
     /// </summary>
     public class MapDrawFilterHelper : IPersistable
     {
+        const bool _defaultDrawBackground = true;
+        const bool _defaultDrawBackgroundMapGrhs = true;
+        const bool _defaultDrawCharacters = true;
+        const bool _defaultDrawForegroundMapGrhs = true;
+        const bool _defaultDrawItems = true;
+        const bool _defaultMapGrhsOnDefaultDepthOnly = false;
+        const bool _defaultMapGrhsOnDefaultLayerOnly = false;
         const string _generalCategoryName = "Draw Filters";
         const string _mapGrhCategoryName = "Draw Filters - MapGrh";
 
-        const bool _defaultDrawBackground = true;
-        const bool _defaultDrawCharacters = true;
-        const bool _defaultDrawItems = true;
-        const bool _defaultDrawForegroundMapGrhs = true;
-        const bool _defaultDrawBackgroundMapGrhs = true;
-        static readonly int? _defaultMapGrhMinDepth = null;
         static readonly int? _defaultMapGrhMaxDepth = null;
+        static readonly int? _defaultMapGrhMinDepth = null;
 
         readonly Func<IDrawable, bool> _filterFunc;
+
+        short _mapGrh_DefaultDepth_Cache;
+        bool _mapGrh_DefaultIsForeground_Cache;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MapDrawFilterHelper"/> class.
@@ -48,8 +55,15 @@ namespace DemoGame.Editor
             DrawItems = _defaultDrawItems;
             DrawForegroundMapGrhs = _defaultDrawForegroundMapGrhs;
             DrawBackgroundMapGrhs = _defaultDrawBackgroundMapGrhs;
+
             MapGrhMinDepth = _defaultMapGrhMinDepth;
             MapGrhMaxDepth = _defaultMapGrhMaxDepth;
+            MapGrhsOnDefaultLayerOnly = _defaultMapGrhsOnDefaultLayerOnly;
+            MapGrhsOnDefaultDepthOnly = _defaultMapGrhsOnDefaultDepthOnly;
+
+            _mapGrh_DefaultDepth_Cache = EditorSettings.Default.MapGrh_DefaultDepth;
+            _mapGrh_DefaultIsForeground_Cache = EditorSettings.Default.MapGrh_DefaultIsForeground;
+            EditorSettings.Default.PropertyChanged += EditorSettings_PropertyChanged;
         }
 
         /// <summary>
@@ -64,28 +78,6 @@ namespace DemoGame.Editor
         public bool DrawBackground { get; set; }
 
         /// <summary>
-        /// Gets or sets if the <see cref="Character"/>s are drawn.
-        /// The default value is true.
-        /// </summary>
-        [SyncValue]
-        [Category(_generalCategoryName)]
-        [DefaultValue(_defaultDrawCharacters)]
-        [Browsable(true)]
-        [Description("If characters on the map are drawn.")]
-        public bool DrawCharacters { get; set; }
-
-        /// <summary>
-        /// Gets or sets if the <see cref="ItemEntityBase"/>s are drawn.
-        /// The default value is true.
-        /// </summary>
-        [SyncValue]
-        [Category(_generalCategoryName)]
-        [DefaultValue(_defaultDrawItems)]
-        [Browsable(true)]
-        [Description("If items on the map are drawn.")]
-        public bool DrawItems { get; set; }
-
-        /// <summary>
         /// Gets or sets if the background <see cref="MapGrh"/>s are drawn.
         /// The default value is true.
         /// </summary>
@@ -95,6 +87,17 @@ namespace DemoGame.Editor
         [Browsable(true)]
         [Description("If the background MapGrhs are drawn.")]
         public bool DrawBackgroundMapGrhs { get; set; }
+
+        /// <summary>
+        /// Gets or sets if the <see cref="Character"/>s are drawn.
+        /// The default value is true.
+        /// </summary>
+        [SyncValue]
+        [Category(_generalCategoryName)]
+        [DefaultValue(_defaultDrawCharacters)]
+        [Browsable(true)]
+        [Description("If characters on the map are drawn.")]
+        public bool DrawCharacters { get; set; }
 
         /// <summary>
         /// Gets or sets if the foreground <see cref="MapGrh"/>s are drawn.
@@ -108,15 +111,25 @@ namespace DemoGame.Editor
         public bool DrawForegroundMapGrhs { get; set; }
 
         /// <summary>
-        /// Gets or sets the minimum layer depth of the <see cref="MapGrh"/>s to draw.
-        /// Default value is null.
+        /// Gets or sets if the <see cref="ItemEntityBase"/>s are drawn.
+        /// The default value is true.
         /// </summary>
         [SyncValue]
-        [Category(_mapGrhCategoryName)]
-        [DefaultValue(null)]
+        [Category(_generalCategoryName)]
+        [DefaultValue(_defaultDrawItems)]
         [Browsable(true)]
-        [Description("The minimum layer depth of the MapGrhs to draw.")]
-        public int? MapGrhMinDepth { get; set; }
+        [Description("If items on the map are drawn.")]
+        public bool DrawItems { get; set; }
+
+        /// <summary>
+        /// Gets a Func describing this filter.
+        /// </summary>
+        /// <returns>A Func describing this filter.</returns>
+        [Browsable(false)]
+        public Func<IDrawable, bool> FilterFunc
+        {
+            get { return _filterFunc; }
+        }
 
         /// <summary>
         /// Gets or sets the maximum layer depth of the <see cref="MapGrh"/>s to draw.
@@ -130,13 +143,49 @@ namespace DemoGame.Editor
         public int? MapGrhMaxDepth { get; set; }
 
         /// <summary>
-        /// Gets a Func describing this filter.
+        /// Gets or sets the minimum layer depth of the <see cref="MapGrh"/>s to draw.
+        /// Default value is null.
         /// </summary>
-        /// <returns>A Func describing this filter.</returns>
-        [Browsable(false)]
-        public Func<IDrawable, bool> FilterFunc
+        [SyncValue]
+        [Category(_mapGrhCategoryName)]
+        [DefaultValue(null)]
+        [Browsable(true)]
+        [Description("The minimum layer depth of the MapGrhs to draw.")]
+        public int? MapGrhMinDepth { get; set; }
+
+        /// <summary>
+        /// Gets or sets if <see cref="MapGrh"/>s only on the default layer depth will be drawn.
+        /// Default value is false.
+        /// </summary>
+        [SyncValue]
+        [Category(_mapGrhCategoryName)]
+        [DefaultValue(_defaultMapGrhsOnDefaultDepthOnly)]
+        [Browsable(true)]
+        [Description("When true, only the MapGrhs on the default layer depth will be drawn.")]
+        public bool MapGrhsOnDefaultDepthOnly { get; set; }
+
+        /// <summary>
+        /// Gets or sets if <see cref="MapGrh"/>s only on the default layer will be drawn.
+        /// Default value is false.
+        /// </summary>
+        [SyncValue]
+        [Category(_mapGrhCategoryName)]
+        [DefaultValue(_defaultMapGrhsOnDefaultLayerOnly)]
+        [Browsable(true)]
+        [Description("When true, only the MapGrhs on the default layer will be drawn.")]
+        public bool MapGrhsOnDefaultLayerOnly { get; set; }
+
+        /// <summary>
+        /// Handles the PropertyChanged event of the EditorSettings control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.ComponentModel.PropertyChangedEventArgs"/> instance containing the event data.</param>
+        void EditorSettings_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            get { return _filterFunc; }
+            if (StringComparer.Ordinal.Equals("MapGrh_DefaultDepth", e.PropertyName))
+                _mapGrh_DefaultDepth_Cache = EditorSettings.Default.MapGrh_DefaultDepth;
+            else if (StringComparer.Ordinal.Equals("MapGrh_DefaultIsForeground", e.PropertyName))
+                _mapGrh_DefaultIsForeground_Cache = EditorSettings.Default.MapGrh_DefaultIsForeground;
         }
 
         /// <summary>
@@ -146,38 +195,68 @@ namespace DemoGame.Editor
         /// <returns>True if the <paramref name="drawable"/> should be drawn; otherwise false.</returns>
         public bool Filter(IDrawable drawable)
         {
-            // Background layer
-            if (!DrawBackground && drawable.MapRenderLayer == MapRenderLayer.Background)
-                return false;
-
-            // Character layer
-            if (!DrawCharacters && drawable.MapRenderLayer == MapRenderLayer.Chararacter)
-                return false;
-
-            // Item layer
-            if (!DrawItems && drawable.MapRenderLayer == MapRenderLayer.Item)
-                return false;
-
-            // MapGrhs
             if (drawable is MapGrh)
             {
+                // MapGrhs
                 var mg = (MapGrh)drawable;
-                if (mg.IsForeground)
+
+                // Layer
+                if (MapGrhsOnDefaultLayerOnly)
                 {
-                    if (!DrawForegroundMapGrhs)
+                    if (mg.IsForeground != _mapGrh_DefaultIsForeground_Cache)
                         return false;
                 }
                 else
                 {
-                    if (!DrawBackgroundMapGrhs)
-                        return false;
+                    if (mg.IsForeground)
+                    {
+                        if (!DrawForegroundMapGrhs)
+                            return false;
+                    }
+                    else
+                    {
+                        if (!DrawBackgroundMapGrhs)
+                            return false;
+                    }
                 }
 
-                if (MapGrhMinDepth.HasValue && mg.LayerDepth < MapGrhMinDepth.Value)
-                    return false;
+                // Depth
+                if (MapGrhsOnDefaultDepthOnly)
+                {
+                    if (mg.LayerDepth != _mapGrh_DefaultDepth_Cache)
+                        return false;
+                }
+                else
+                {
+                    if (MapGrhMinDepth.HasValue && mg.LayerDepth < MapGrhMinDepth.Value)
+                        return false;
 
-                if (MapGrhMaxDepth.HasValue && mg.LayerDepth > MapGrhMaxDepth.Value)
-                    return false;
+                    if (MapGrhMaxDepth.HasValue && mg.LayerDepth > MapGrhMaxDepth.Value)
+                        return false;
+                }
+            }
+            else
+            {
+                switch (drawable.MapRenderLayer)
+                {
+                    case MapRenderLayer.Background:
+                        // Background layer
+                        if (!DrawBackground)
+                            return false;
+                        break;
+
+                    case MapRenderLayer.Chararacter:
+                        // Character layer
+                        if (!DrawCharacters)
+                            return false;
+                        break;
+
+                    case MapRenderLayer.Item:
+                        // Item layer
+                        if (!DrawItems)
+                            return false;
+                        break;
+                }
             }
 
             return true;
