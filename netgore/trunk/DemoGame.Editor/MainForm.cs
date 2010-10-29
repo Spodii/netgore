82fda2b440fa0b1c1309ec4c4eea5e38e19270fb
mr.spodi <spodii@hotmail.com>
2010-10-29 21:21:32 +0000

    Forgot to remove some temp code.

--- netgore/trunk/DemoGame.Editor/MainForm.cs
+++ netgore/trunk/DemoGame.Editor/MainForm.cs
@@ -131,11 +131,6 @@ namespace DemoGame.Editor
 
             // Load the settings
             LoadDockSettings("User");
-
-            // TODO: !!
-            var editorFrm = new EditMapForm();
-            editorFrm.MapScreenControl.ChangeMap(new MapID(3));
-            editorFrm.Show(dockPanel);
         }
 
         void SaveDockSettings(string settingsName)
