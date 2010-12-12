d3d6a747a2c0b2bef74b2b6855346acdfcac9e16
aphro.cag <sam_giles2001@yahoo.co.uk>
2010-12-12 23:43:28 +0000

    Resolves issue 238.

--- netgore/trunk/Tools/GoreUpdater/GoreUpdater.Client/Program.cs
+++ netgore/trunk/Tools/GoreUpdater/GoreUpdater.Client/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
 using log4net;
+using System.Diagnostics;
 
 namespace GoreUpdater
 {
@@ -14,13 +15,41 @@ namespace GoreUpdater
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            log.Info("Starting GoreUpdater client...");
+            bool runUpdater = false;
+            foreach (string arg in args)
+            {
+                if (arg == "-runUpdater")
+                    runUpdater = true;
+            }
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (runUpdater)
+            {
+
+                log.Info("Starting GoreUpdater client...");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            else
+            {
+                try
+                {
+                    
+                    ProcessStartInfo procInfo = new ProcessStartInfo();
+                    procInfo.UseShellExecute = true;
+                    procInfo.FileName = Application.ExecutablePath;  //The file in that DIR.
+                    procInfo.WorkingDirectory = ""; //The working DIR.
+                    procInfo.Verb = "runas";
+                    procInfo.Arguments = "-runUpdater";
+                    Process.Start(procInfo);  //Start that process.
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }          
+            }
         }
     }
 }
