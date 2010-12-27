b935272f6fb8926f485d483701a5c6e531439439
mr.spodi <spodii@hotmail.com>
2010-12-27 03:06:26 +0000

    Documentation

--- netgore/trunk/DemoGame/Body/BodyInfo.cs
+++ netgore/trunk/DemoGame/Body/BodyInfo.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using NetGore;
+using NetGore.Graphics;
 using NetGore.IO;
 using SFML.Graphics;
 
@@ -43,20 +44,72 @@ namespace DemoGame
         {
         }
 
+        /// <summary>
+        /// Gets the name of the <see cref="SkeletonBody"/> (when using <see cref="SkeletonCharacterSprite"/>) or
+        /// sprite category (when using <see cref="GrhCharacterSprite"/>) to use for this body.
+        /// </summary>
         public string Body { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the <see cref="SkeletonSet"/> (when using <see cref="SkeletonCharacterSprite"/>) or
+        /// sprite category (when using <see cref="GrhCharacterSprite"/>) to use for this body when drawing
+        /// the character is falling.
+        /// </summary>
         public string Fall { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="BodyID"/> of this body.
+        /// </summary>
         public BodyID ID { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the <see cref="SkeletonSet"/> (when using <see cref="SkeletonCharacterSprite"/>) or
+        /// sprite category (when using <see cref="GrhCharacterSprite"/>) to use for this body when drawing
+        /// the character is jumping.
+        /// </summary>
         public string Jump { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the <see cref="SkeletonSet"/> (when using <see cref="SkeletonCharacterSprite"/>) or
+        /// sprite category (when using <see cref="GrhCharacterSprite"/>) to use for this body when drawing
+        /// the character is punching.
+        /// </summary>
         public string Punch { get; private set; }
+
+        /// <summary>
+        /// Gets the size of the body in pixels. While this should be roughly equal to the size of the body when
+        /// drawn, it is not required at all.
+        /// </summary>
         public Vector2 Size { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the <see cref="SkeletonSet"/> (when using <see cref="SkeletonCharacterSprite"/>) or
+        /// sprite category (when using <see cref="GrhCharacterSprite"/>) to use for this body when drawing
+        /// the character is standing still.
+        /// </summary>
         public string Stand { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the <see cref="SkeletonSet"/> (when using <see cref="SkeletonCharacterSprite"/>) or
+        /// sprite category (when using <see cref="GrhCharacterSprite"/>) to use for this body when drawing
+        /// the character is walking.
+        /// </summary>
         public string Walk { get; private set; }
 
+        /// <summary>
+        /// Reads a <see cref="BodyInfo"/> from an <see cref="IValueReader"/>.
+        /// </summary>
+        /// <param name="reader">The <see cref="IValueReader"/> to read from.</param>
+        /// <returns>The read <see cref="BodyInfo"/>.</returns>
         public static BodyInfo Read(IValueReader reader)
         {
             return new BodyInfo(reader);
         }
 
+        /// <summary>
+        /// Writes this <see cref="BodyInfo"/> to an <see cref="IValueWriter"/>.
+        /// </summary>
+        /// <param name="writer">The <see cref="IValueWriter"/> to write to.</param>
         public void Write(IValueWriter writer)
         {
             writer.Write(_idValueKey, ID);
