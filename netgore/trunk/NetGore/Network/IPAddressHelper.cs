2a5f018bd7f57ad2d968f9741d12e2ad4ae6d5e5
Darkfrost101@gmail.com <d@rkfrost.info>
2012-05-18 20:00:25 +0000

    Removed whatismyip.org completely, since they were nice enough to change their site to return the ip as an image. :p

--- netgore/trunk/NetGore/Network/IPAddressHelper.cs
+++ netgore/trunk/NetGore/Network/IPAddressHelper.cs
@@ -29,7 +29,6 @@ namespace NetGore.Network
             _parsers = new ExternalIPParser[]
             {
                 new ExternalIPParser(new Uri("http://checkip.dyndns.org/"), rStraightCheck),
-                new ExternalIPParser(new Uri("http://whatismyip.org/"), rStraightCheck),
                 new ExternalIPParser(new Uri("http://www.faqs.org/ip.php"), rFaqs),
                 new ExternalIPParser(new Uri("http://www.checkmyip.com/"), rCheckMyIP),
                 new ExternalIPParser(new Uri("http://icanhazip.com/"), null)
