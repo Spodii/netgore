be17844b668e66bc8b9f745a1ab1312957240aa1
Spodiii@gmail.com <spodii@hotmail.com>
2012-12-04 16:14:56 +0000

    Improve ByteArrayEqualityComparer, and add related unit tests

---
+++ netgore/trunk/NetGore.Tests/NetGore/ByteArrayEqualityComparerTests.cs
@@ -0,0 +1,132 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NetGore.Tests.NetGore
+{
+    [TestFixture]
+    public class ByteArrayEqualityComparerTests
+    {
+        [Test]
+        public void DifferentLengthTest()
+        {
+            byte[] a = new byte [] { 1, 2, 3, 4, 5 };
+            byte[] b = new byte [] { 1, 2, 3, 4, 5, 6 };
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void NullTest1()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 4, 5 };
+            byte[] b = null;
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void NullTest2()
+        {
+            byte[] a = null;
+            byte[] b = new byte[] { 1, 2, 3, 4, 5, 6 };
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void NullTest3()
+        {
+            byte[] a = null;
+            byte[] b = null;
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void EmptyTest1()
+        {
+            byte[] a = new byte[0];
+            byte[] b = new byte[] { 1, 2, 3, 4, 5, 6 };
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void EmptyTest2()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 4, 5 };
+            byte[] b = new byte[0]; 
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void EmptyTest3()
+        {
+            byte[] a = new byte[0];
+            byte[] b = new byte[0]; 
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void SameLengthTest()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 4 };
+            byte[] b = new byte[] { 1, 2, 4, 4 };
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void SameTest1()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 4, 5, 6, 1, 3, 5 };
+            byte[] b = a;
+            bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void RandomEqualTest()
+        {
+            SafeRandom rnd = new SafeRandom();
+            for (int loop = 0; loop < 100000; loop++)
+            {
+                byte[] a = new byte[rnd.Next(1, 100)];
+                rnd.NextBytes(a);
+
+                byte[] b = DeepCopy(a);
+
+                bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+                Assert.IsTrue(actual);
+            }
+        }
+
+        [Test]
+        public void RandomNotEqualTest()
+        {
+            SafeRandom rnd = new SafeRandom();
+            for (int loop = 0; loop < 1000000; loop++)
+            {
+                byte[] a = new byte[rnd.Next(1, 100)];
+                rnd.NextBytes(a);
+
+                byte[] b = DeepCopy(a);
+
+                b[rnd.Next(0, b.Length)]++; // Increment at a random index
+
+                bool actual = ByteArrayEqualityComparer.AreEqual(a, b);
+                Assert.IsFalse(actual);
+            }
+        }
+
+        static byte[] DeepCopy(byte[] a)
+        {
+            byte[] b = new byte[a.Length];
+            for (int i = 0; i < a.Length; i++)
+                b[i] = a[i];
+            return b;
+        }
+    }
+}
--- netgore/trunk/NetGore/Core/ByteArrayEqualityComparer.cs
+++ netgore/trunk/NetGore/Core/ByteArrayEqualityComparer.cs
@@ -41,18 +41,66 @@ namespace NetGore
         /// <returns>
         /// true if the specified objects are equal; otherwise, false.
         /// </returns>
-        public static bool AreEqual(byte[] x, byte[] y)
+        public static unsafe bool AreEqual(byte[] x, byte[] y)
         {
+            if (x == y)
+                return true;
+
             if (x == null || y == null || x.Length != y.Length)
                 return false;
 
+            /*
+            // Simple (slower) version:
             for (var i = 0; i < x.Length; i++)
             {
                 if (x[i] != y[i])
                     return false;
             }
+            */
 
-            return true;
+            // Do comparison using multiple bytes at a time to increase performance
+            fixed (byte* px = x)
+            {
+                fixed (byte* py = y)
+                {
+                    byte* x1 = px;
+                    byte* x2 = py;
+                    int len = x.Length;
+
+                    // Compare 64-bit
+                    for (int i = 0; i < len / 8; i++, x1 += 8, x2 += 8)
+                    {
+                        if (*((long*)x1) != *((long*)x2)) return false;
+                    }
+
+                    // Compare 32-bit
+                    if ((len & 4) != 0) 
+                    { 
+                        if (*((int*)x1) != *((int*)x2)) 
+                            return false; 
+                        x1 += 4; 
+                        x2 += 4; 
+                    }
+
+                    // Compare 16-bit
+                    if ((len & 2) != 0) 
+                    { 
+                        if (*((short*)x1) != *((short*)x2)) 
+                            return false; 
+                        x1 += 2; 
+                        x2 += 2; 
+                    }
+
+                    // Compare 8-bit
+                    if ((len & 1) != 0)
+                    {
+                        if (*((byte*)x1) != *((byte*)x2))
+                            return false;
+                    }
+
+                    return true;
+                }
+            }
         }
 
         /// <summary>
